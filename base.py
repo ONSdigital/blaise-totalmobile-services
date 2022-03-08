49d45873b78b97d7e6ef3d647b90dcff217efa12
Sam Bryant <srbry@hotmail.com>
2022-03-08 11:28:57 +0000

    Refactor token logic to a base client

---
+++ base.py
@@ -0,0 +1,39 @@
+import requests
+from datetime import datetime, timedelta
+
+
+class BaseClient:
+    def __init__(this, url: str, instance: str, client_id, client_secret):
+        this._url = url
+        this._instance = instance
+        this._client_id = client_id
+        this._client_secret = client_secret
+        this.__expires_at = 0
+        this.__access_token = ""
+
+    def get(this, path: str):
+        return requests.get(
+            f"{this._url}/{this._instance}/{path}",
+            headers={"Authorization": f"Bearer {this.__get_token()}"},
+        )
+
+    def get_token(this) -> str:
+        if this.__token_expired():
+            response = requests.post(
+                f"{this._url}/{this._instance}/identity/connect/token",
+                data={
+                    "grant_type": "client_credentials",
+                    "client_id": this._client_id,
+                    "client_secret": this._client_secret,
+                    "scope": "optimiseApi",
+                },
+            )
+            access_token = response.json()
+            this.__access_token = access_token["access_token"]
+            this.__expires_at = datetime.now() + timedelta(access_token["expires_in"])
+        return this.__access_token
+
+    def __token_expired(this) -> bool:
+        if this.__expires_at == 0:
+            return True
+        return datetime.now() >= this.__expires_at
--- optimise.py
+++ optimise.py
@@ -1,48 +1,26 @@
 from datetime import datetime, timedelta
 import requests
 
+from base import BaseClient
 
-class OptimiseClient:
+
+class OptimiseClient(BaseClient):
     def __init__(this, url: str, instance: str, client_id, client_secret):
-        this.url = url
-        this.instance = instance
-        this.client_id = client_id
-        this.client_secret = client_secret
-        this.expires_at = 0
-        this.access_token = ""
+        this._url = url
+        this._instance = instance
+        super().__init__(this._url, this._instance, client_id, client_secret)
 
     def get_jobs(this, world_id: str):
-        return this.__get(f"worlds/{world_id}/jobs").json()
+        return this.get(f"worlds/{world_id}/jobs").json()
 
     def get_worlds(this):
-        return this.__get("worlds").json()
+        return this.get("worlds").json()
 
     def get_world(this, world: str):
-        return this.__get(f"worlds/{world}").json()
+        return this.get(f"worlds/{world}").json()
 
-    def __get(this, path: str):
+    def get(this, path: str):
         return requests.get(
-            f"{this.url}/{this.instance}/api/optimise/{path}",
-            headers={"Authorization": f"Bearer {this.__get_token()}"},
+            f"{this._url}/{this._instance}/api/optimise/{path}",
+            headers={"Authorization": f"Bearer {this.get_token()}"},
         )
-
-    def __get_token(this) -> str:
-        if this.__token_expired():
-            response = requests.post(
-                f"{this.url}/{this.instance}/identity/connect/token",
-                data={
-                    "grant_type": "client_credentials",
-                    "client_id": this.client_id,
-                    "client_secret": this.client_secret,
-                    "scope": "optimiseApi",
-                },
-            )
-            access_token = response.json()
-            this.access_token = access_token["access_token"]
-            this.expires_at = datetime.now() + timedelta(access_token["expires_in"])
-        return this.access_token
-
-    def __token_expired(this) -> bool:
-        if this.expires_at == 0:
-            return True
-        return datetime.now() >= this.expires_at
