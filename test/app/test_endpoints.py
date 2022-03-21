022e4fde09ac38e084fd230ccd45fc9e71ad795f
elthorne <46898565+elthorne@users.noreply.github.com>
2022-03-21 17:51:00 +0000

    refactor: Receive data from TotalMobile POC.
    Post-merge with BLAIS5-3077:
    * Removed app.py
    * Renamed my endpoint tests.
    * Removed my tests/conftest.py and combined it test/conftest.py
    * Added to gitignore.
    
    Reviewed-by: El
    Refs: BLAIS5-3071

--- test/app/test_endpoints.py
+++ test/app/test_endpoints.py
@@ -14,11 +14,11 @@ def test_update_visit_status_request_returns_200(client, upload_visit_status_req
     assert response.status_code == 200
 
 
-def test_submit_form_result_request(client, submit_form_result_request_sample):
+def test_submit_form_result_request_returns_200(client, submit_form_result_request_sample):
     response = client.post("/ons/totalmobile-incoming/SubmitFormResultRequest", json=submit_form_result_request_sample)
     assert response.status_code == 200
 
 
-def test_complete_visit_request(client, complete_visit_request_sample):
+def test_complete_visit_request_returns_200(client, complete_visit_request_sample):
     response = client.post("/ons/totalmobile-incoming/CompleteVisitRequest", json=complete_visit_request_sample)
     assert response.status_code == 200
