ab7b66ff55f99c7a1e8ec2b0e502dad72cc1a774
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-03-28 16:52:47 +0100

    feature: create entry point for cloud function
    Reviewed-by: James & Jamie
    Refs: BLAIS5-3082

--- cloud_functions/create_questionnaire_case_tasks.py
+++ cloud_functions/create_questionnaire_case_tasks.py
@@ -1,20 +1,21 @@
 from datetime import timedelta
-from typing import List
+from typing import List, Dict
 from google.protobuf.duration_pb2 import Duration
 from appconfig import Config
 from uuid import uuid4
 from models.totalmobile_job_model import TotalmobileJobModel
 from google.cloud import tasks_v2
+from client.optimise import OptimiseClient
+
 
 import blaise_restapi
 import json
 
 
-def retrieve_case_data(questionnaire_name: str) -> list[str]:
-    config = Config.from_env()
+def retrieve_case_data(questionnaire_name: str, config: Config) -> Dict[str, str]:
     restapi_client = blaise_restapi.Client(config.blaise_api_url)
 
-    cases = restapi_client.get_instrument_data(
+    instrument_data = restapi_client.get_instrument_data(
         config.blaise_server_park,
         questionnaire_name,
         [
@@ -32,13 +33,29 @@ def retrieve_case_data(questionnaire_name: str) -> list[str]:
             "qiD.Serial_Number",
         ],
     )
+    return instrument_data["reportingData"]
 
 
-
+def retrieve_world_id(config: Config) -> str:
+    optimise_client = OptimiseClient(
+        config.totalmobile_url,
+        config.totalmobile_instance,
+        config.totalmobile_client_id,
+        config.totalmobile_client_secret,
+    )
+    world = "Region 1"
+    return optimise_client.get_world(world)["id"]
 
 
 def create_task_name(job_model: TotalmobileJobModel) -> str:
     return f"{job_model.questionnaire_name}-{job_model.case_data['qiD.Serial_Number']}-{str(uuid4())}"
+
+
+def map_totalmobile_job_models(cases: Dict[str, str], world_id: str, questionnaire_name: str) -> list[TotalmobileJobModel]:
+    jobs = []
+    for case in cases:
+        jobs.append(TotalmobileJobModel(questionnaire_name, world_id, case))
+    return jobs
 
 
 def prepare_tasks(*job_models: TotalmobileJobModel) -> List[tasks_v2.CreateTaskRequest]:
@@ -67,3 +84,16 @@ def prepare_tasks(*job_models: TotalmobileJobModel) -> List[tasks_v2.CreateTaskR
         task_requests.append(request)
     return task_requests
 
+def create_case_tasks_for_questionnaire(questionnaire_name: str) -> None:
+    # TODO
+    # filter on business logic for week 1 and week 2 criteria
+    # push to queue
+
+    config = Config.from_env()
+    world_id = retrieve_world_id(config)
+
+    cases = retrieve_case_data(questionnaire_name, config)
+    filtered_cases = cases
+
+    totalmobile_job_models = map_totalmobile_job_models(cases, world_id, questionnaire_name)
+    total_mobile_job_tasks = prepare_tasks(totalmobile_job_models)
--- tests/cloud_functions/test_create_questionnaire_case_tasks.py
+++ tests/cloud_functions/test_create_questionnaire_case_tasks.py
@@ -1,8 +1,12 @@
 from appconfig import Config
-from cloud_functions.create_questionnaire_case_tasks import create_task_name, prepare_tasks
+from cloud_functions.create_questionnaire_case_tasks import create_task_name, prepare_tasks, retrieve_case_data, \
+    retrieve_world_id, map_totalmobile_job_models
 from models.totalmobile_job_model import TotalmobileJobModel
 from unittest.mock import patch
+from client.optimise import OptimiseClient
+
 import json
+import blaise_restapi
 
 
 def test_create_task_name_returns_correct_name_when_called():
@@ -49,7 +53,8 @@ def test_prepare_tasks_returns_an_expected_number_of_tasks_when_given_a_list_of_
 @patch.object(Config, "from_env")
 def test_prepare_tasks_returns_expected_tasks_when_given_a_list_of_job_models(_mock_config_from_env):
     # arrange
-    _mock_config_from_env.return_value = Config("", "", "", "", "totalmobile_jobs_queue_id", "cloud_function", "project", "region", "rest_api_url", "gusty")
+    _mock_config_from_env.return_value = Config("", "", "", "", "totalmobile_jobs_queue_id", "cloud_function",
+                                                "project", "region", "rest_api_url", "gusty")
 
     model1 = TotalmobileJobModel({"qiD.Serial_Number": "90001"}, "OPN2101A", "world")
     model2 = TotalmobileJobModel({"qiD.Serial_Number": "90002"}, "OPN2101A", "world")
@@ -68,3 +73,93 @@ def test_prepare_tasks_returns_expected_tasks_when_given_a_list_of_job_models(_m
     assert result[1].task.http_request.url == "https://region-project.cloudfunctions.net/cloud_function"
     assert result[1].task.http_request.body == json.dumps(model2.case_data).encode()
 
+
+@patch.object(blaise_restapi.Client, "get_instrument_data")
+def test_retrieve_case_data_calls_the_rest_api_client_with_the_correct_parameters(_mock_rest_api_client):
+    # arrange
+    config = Config("", "", "", "", "", "", "", "", "rest_api_url", "gusty")
+    _mock_rest_api_client.return_value = {"instrumentName": "DST2106Z",
+                                          "instrumentId": "12345-12345-12345-12345-12345",
+                                          "reportingData": ""}
+
+    blaise_server_park = "gusty"
+    questionnaire_name = "OPN2101A"
+
+    fields = [
+        "qDataBag.UPRN_Latitude",
+        "qDataBag.UPRN_Longitude",
+        "qDataBag.Prem1",
+        "qDataBag.Prem2",
+        "qDataBag.Prem3",
+        "qDataBag.PostTown",
+        "qDataBag.PostCode",
+        "qDataBag.TelNo",
+        "qDataBag.TelNo2",
+        "hOut",
+        "srvStat",
+        "qiD.Serial_Number",
+    ]
+
+    # act
+    retrieve_case_data(questionnaire_name, config)
+
+    # assert
+    _mock_rest_api_client.assert_called_with(blaise_server_park, questionnaire_name, fields)
+
+
+@patch.object(blaise_restapi.Client, "get_instrument_data")
+def test_retrieve_case_data_returns_the_case_data_supplied_by_the_rest_api_client(_mock_rest_api_client):
+    # arrange
+    config = Config("", "", "", "", "", "", "", "", "rest_api_url", "gusty")
+    _mock_rest_api_client.return_value = {"instrumentName": "DST2106Z",
+                                          "instrumentId": "12345-12345-12345-12345-12345",
+                                          "reportingData": [{"qiD.Serial_Number": "10010", "qhAdmin.HOut": "110"},
+                                                            {"qiD.Serial_Number": "10020", "qhAdmin.HOut": "110"},
+                                                            {"qiD.Serial_Number": "10030", "qhAdmin.HOut": "110"}
+                                                            ]}
+    questionnaire_name = "OPN2101A"
+
+    # act
+    result = retrieve_case_data(questionnaire_name, config)
+
+    # assert
+    assert result == [{"qiD.Serial_Number": "10010", "qhAdmin.HOut": "110"},
+                      {"qiD.Serial_Number": "10020", "qhAdmin.HOut": "110"},
+                      {"qiD.Serial_Number": "10030", "qhAdmin.HOut": "110"}]
+
+
+@patch.object(OptimiseClient, "get_world")
+def test_retrieve_world_id_returns_a_world_id(_mock_optimise_client):
+    # arrange
+    config = Config("totalmobile_url", "totalmobile_instance", "totalmobile_client_id",
+                    "totalmobile_client_secret", "", "", "", "", "rest_api_url", "gusty")
+    _mock_optimise_client.return_value = {
+        "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "identity": {
+            "reference": "test"
+        },
+        "type": "foo"
+    }
+
+    # act
+    result = retrieve_world_id(config)
+
+    # assert
+    assert result == "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+
+
+def test_map_totalmobile_job_models_maps_the_correct_list_of_models():
+    # arrange
+    questionnaire_name = "OPN2101A"
+    world_id = "foo"
+
+    case_data = [{"qiD.Serial_Number": "10010", "qhAdmin.HOut": "110"},
+                 {"qiD.Serial_Number": "10020", "qhAdmin.HOut": "120"},
+                 {"qiD.Serial_Number": "10030", "qhAdmin.HOut": "130"}]
+    # act
+    result = map_totalmobile_job_models(case_data, world_id, questionnaire_name)
+
+    # assert
+    assert result == [TotalmobileJobModel("OPN2101A", "foo", {"qiD.Serial_Number": "10010", "qhAdmin.HOut": "110"}),
+                      TotalmobileJobModel("OPN2101A", "foo", {"qiD.Serial_Number": "10020", "qhAdmin.HOut": "120"}),
+                      TotalmobileJobModel("OPN2101A", "foo", {"qiD.Serial_Number": "10030", "qhAdmin.HOut": "130"})]
