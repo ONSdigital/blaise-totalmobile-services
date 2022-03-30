1b54d30c0438cc062671f121cb1b44069f08022e
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-03-30 15:41:39 +0100

    feature: add entry point in main
    Reviewed-by: James & Jamie
    Refs: BLAIS5-3082

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import flask
 import os
 
 from cloud_functions.create_job import create_totalmobile_job
+from cloud_functions.create_instrument_case_tasks import
 from dotenv import load_dotenv
 
 if os.path.isfile("./.env"):
@@ -9,5 +10,9 @@ if os.path.isfile("./.env"):
     load_dotenv()
 
 
-def CreateTMJob(request: flask.Request) -> str:
+def create_totalmobile_job(request: flask.Request) -> str:
     return create_totalmobile_job(request)
+
+
+def create_instrument_case_tasks(request: flask.Request) -> str:
+    return create_case_tasks_for_instrument(request)
