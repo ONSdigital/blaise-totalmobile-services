430b6a0c2fd8c98556a9b2f32ff1d5be294b8fb2
elthorne <46898565+elthorne@users.noreply.github.com>
2022-04-07 15:31:37 +0100

    refactor: Process data placeholder for SubmitFormResultRequest
    Added typing
    
    Reviewed-by: El, Tom, Callum
    Refs: BLAIS5-3086

--- app/services/total_mobile_service.py
+++ app/services/total_mobile_service.py
@@ -3,7 +3,7 @@ import blaise_restapi
 from appconfig import Config
 
 
-def update_case_telephone_number(instrument_name, case_id, telephone_number):
+def update_case_telephone_number(instrument_name: str, case_id: str, telephone_number: str) -> None:
     print(f"Updating telephone number for {instrument_name}, {case_id}, please wait...")
     config = Config.from_env()
     restapi_client = blaise_restapi.Client(config.blaise_api_url)
@@ -12,6 +12,6 @@ def update_case_telephone_number(instrument_name, case_id, telephone_number):
     restapi_client.patch_case_data(config.blaise_server_park, instrument_name, case_id, data_fields)
 
 
-def do_something_service(reference):
+def do_something_service(reference: str) -> None:
     print(f"The reference number is: {reference}")
     print("There are no solid requirements for this feature. You shall not pass!")
