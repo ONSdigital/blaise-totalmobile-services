ddfe38503f206be2ac617d05991c279786f4cc84
elthorne <46898565+elthorne@users.noreply.github.com>
2022-04-07 15:34:29 +0100

    refactor: Process data placeholder for SubmitFormResultRequest
    Remove redundant dependency.
    
    Reviewed-by: El, Tom, Callum
    Refs: BLAIS5-3086

--- app/services/total_mobile_service.py
+++ app/services/total_mobile_service.py
@@ -7,8 +7,10 @@ def update_case_telephone_number(instrument_name: str, case_id: str, telephone_n
     print(f"Updating telephone number for {instrument_name}, {case_id}, please wait...")
     config = Config.from_env()
     restapi_client = blaise_restapi.Client(config.blaise_api_url)
+    print(f"\nrest_api: {restapi_client}")
 
     data_fields = {"qDataBag.TelNo": telephone_number}
+    print(f"\ndata_fields: {data_fields}")
     restapi_client.patch_case_data(config.blaise_server_park, instrument_name, case_id, data_fields)
 
 
