13ccb1e6efe0cbb32fe02002cc8724aae6aad615
calnic <98323072+cal-nic@users.noreply.github.com>
2022-07-14 16:18:42 +0100

    improved prepare task tests

--- tests/cloud_functions/test_functions.py
+++ tests/cloud_functions/test_functions.py
@@ -7,42 +7,11 @@ from models.totalmobile_job_model import TotalmobileJobModel
 
 
 @mock.patch.object(Config, "from_env")
-def test_prepare_tasks_returns_an_expected_number_of_tasks_when_given_a_list_of_job_models(
-    _mock_config_from_env,
-):
-    # arrange
-    _mock_config_from_env.return_value = Config(
-        "", "", "", "", "", "", "", "", "", "", ""
-    )
-
-    models = [
-        TotalmobileJobModel("OPN2101A", "world", {"qiD.Serial_Number": "90001"}),
-        TotalmobileJobModel("OPN2101A", "world", {"qiD.Serial_Number": "90002"})
-    ]
-
-    tasks = [
-        (create_task_name(job_model), job_model.json().encode())
-        for job_model in models
-    ]
-
-    # act
-    result = prepare_tasks(
-        tasks=tasks,
-        queue_id="foo",
-        cloud_function_name="bar"
-    )
-
-    # assert
-    assert len(result) == 2
-    assert result[0] != result[1]
-
-
-@mock.patch.object(Config, "from_env")
 def test_prepare_tasks_returns_expected_tasks_when_given_a_list_of_job_models(
-    _mock_config_from_env,
+    mock_config_from_env,
 ):
     # arrange
-    _mock_config_from_env.return_value = Config(
+    mock_config_from_env.return_value = Config(
         "",
         "",
         "",
@@ -56,13 +25,7 @@ def test_prepare_tasks_returns_expected_tasks_when_given_a_list_of_job_models(
         "cloud_function_sa",
     )
 
-    model1 = TotalmobileJobModel("OPN2101A", "world", {"qiD.Serial_Number": "90001"})
-    model2 = TotalmobileJobModel("OPN2101A", "world", {"qiD.Serial_Number": "90002"})
-
-    tasks = [
-        (create_task_name(job_model), job_model.json().encode())
-        for job_model in [model1, model2]
-    ]
+    tasks = [("task1", b"task1body"),("task2",b"task2body")]
 
     # act
     result = prepare_tasks(
@@ -73,28 +36,24 @@ def test_prepare_tasks_returns_expected_tasks_when_given_a_list_of_job_models(
 
     # assert
     assert result[0].parent == "totalmobile_jobs_queue_id"
-    assert result[0].task.name.startswith(
-        "totalmobile_jobs_queue_id/tasks/OPN2101A-90001-"
-    )
+    assert result[0].task.name == "totalmobile_jobs_queue_id/tasks/task1"
     assert (
         result[0].task.http_request.url
         == "https://region-project.cloudfunctions.net/cloud_function"
     )
-    assert result[0].task.http_request.body == model1.json().encode()
+    assert result[0].task.http_request.body == b"task1body"
     assert (
         result[0].task.http_request.oidc_token.service_account_email
         == "cloud_function_sa"
     )
 
     assert result[1].parent == "totalmobile_jobs_queue_id"
-    assert result[1].task.name.startswith(
-        "totalmobile_jobs_queue_id/tasks/OPN2101A-90002-"
-    )
+    assert result[1].task.name == "totalmobile_jobs_queue_id/tasks/task2"
     assert (
         result[1].task.http_request.url
         == "https://region-project.cloudfunctions.net/cloud_function"
     )
-    assert result[1].task.http_request.body == model2.json().encode()
+    assert result[1].task.http_request.body == b"task2body"
     assert (
         result[1].task.http_request.oidc_token.service_account_email
         == "cloud_function_sa"
