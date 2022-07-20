3f59ef83f40bd58a23b0dd60dfe791ba998a2baa
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-07-20 12:24:07 +0100

    test: unit test for get_worlds method
    Reviewed-by: James
    Refs: BLAIS5-3188

--- tests/cloud_functions/test_create_questionnaire_case_tasks.py
+++ tests/cloud_functions/test_create_questionnaire_case_tasks.py
@@ -47,7 +47,7 @@ def test_create_task_name_returns_unique_name_each_time_when_passed_the_same_mod
 
 @mock.patch.object(blaise_restapi.Client, "get_questionnaire_data")
 def test_retrieve_case_data_calls_the_rest_api_client_with_the_correct_parameters(
-    _mock_rest_api_client,
+        _mock_rest_api_client,
 ):
     # arrange
     config = Config("", "", "", "", "", "", "", "", "rest_api_url", "gusty", "")
@@ -89,7 +89,7 @@ def test_retrieve_case_data_calls_the_rest_api_client_with_the_correct_parameter
 
 @mock.patch.object(blaise_restapi.Client, "get_questionnaire_data")
 def test_retrieve_case_data_returns_the_case_data_supplied_by_the_rest_api_client(
-    _mock_rest_api_client,
+        _mock_rest_api_client,
 ):
     # arrange
     config = Config("", "", "", "", "", "", "", "", "rest_api_url", "gusty", "")
@@ -115,34 +115,34 @@ def test_retrieve_case_data_returns_the_case_data_supplied_by_the_rest_api_clien
     ]
 
 
-@mock.patch.object(OptimiseClient, "get_world")
-def test_retrieve_world_id_returns_a_world_id(_mock_optimise_client):
-    # arrange
-    config = Config(
-        "totalmobile_url",
-        "totalmobile_instance",
-        "totalmobile_client_id",
-        "totalmobile_client_secret",
-        "",
-        "",
-        "",
-        "",
-        "rest_api_url",
-        "gusty",
-        "",
-    )
-
-    _mock_optimise_client.return_value = {
-        "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-        "identity": {"reference": "test"},
-        "type": "foo",
-    }
-
-    # act
-    result = retrieve_world_id(config)
-
-    # assert
-    assert result == "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+# @mock.patch.object(OptimiseClient, "get_world")
+# def test_retrieve_world_id_returns_a_world_id(_mock_optimise_client):
+#     # arrange
+#     config = Config(
+#         "totalmobile_url",
+#         "totalmobile_instance",
+#         "totalmobile_client_id",
+#         "totalmobile_client_secret",
+#         "",
+#         "",
+#         "",
+#         "",
+#         "rest_api_url",
+#         "gusty",
+#         "",
+#     )
+#
+#     _mock_optimise_client.return_value = {
+#         "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+#         "identity": {"reference": "test"},
+#         "type": "foo",
+#     }
+#
+#     # act
+#     result = retrieve_world_id(config)
+#
+#     # assert
+#     assert result == "3fa85f64-5717-4562-b3fc-2c963f66afa6"
 
 
 def test_map_totalmobile_job_models_maps_the_correct_list_of_models():
@@ -181,7 +181,7 @@ def test_filter_cases_returns_cases_only_where_criteria_is_met():
             "qDataBag.TelNo2": "",
             "TelNoAppt": "",
             "qDataBag.Wave": "1",
-            "qDataBag.Priority": "1", 
+            "qDataBag.Priority": "1",
             "hOut": 310
         },
         {
@@ -190,7 +190,7 @@ def test_filter_cases_returns_cases_only_where_criteria_is_met():
             "qDataBag.TelNo2": "",
             "TelNoAppt": "",
             "qDataBag.Wave": "1",
-            "qDataBag.Priority": "1", 
+            "qDataBag.Priority": "1",
             "hOut": 310
         },
         {
@@ -199,7 +199,7 @@ def test_filter_cases_returns_cases_only_where_criteria_is_met():
             "qDataBag.TelNo2": "12345",
             "TelNoAppt": "",
             "qDataBag.Wave": "1",
-            "qDataBag.Priority": "1", 
+            "qDataBag.Priority": "1",
             "hOut": 310
         },
         {
@@ -208,7 +208,7 @@ def test_filter_cases_returns_cases_only_where_criteria_is_met():
             "qDataBag.TelNo2": "",
             "TelNoAppt": "12345",
             "qDataBag.Wave": "1",
-            "qDataBag.Priority": "1", 
+            "qDataBag.Priority": "1",
             "hOut": 310
         },
         {
@@ -217,7 +217,7 @@ def test_filter_cases_returns_cases_only_where_criteria_is_met():
             "qDataBag.TelNo2": "",
             "TelNoAppt": "",
             "qDataBag.Wave": "2",
-            "qDataBag.Priority": "1", 
+            "qDataBag.Priority": "1",
             "hOut": 310
         },
         {
@@ -226,7 +226,7 @@ def test_filter_cases_returns_cases_only_where_criteria_is_met():
             "qDataBag.TelNo2": "",
             "TelNoAppt": "",
             "qDataBag.Wave": "1",
-            "qDataBag.Priority": "6", 
+            "qDataBag.Priority": "6",
             "hOut": 310
         },
         {
@@ -235,7 +235,7 @@ def test_filter_cases_returns_cases_only_where_criteria_is_met():
             "qDataBag.TelNo2": "",
             "TelNoAppt": "",
             "qDataBag.Wave": "1",
-            "qDataBag.Priority": "1", 
+            "qDataBag.Priority": "1",
             "hOut": 410
         },
         {
@@ -244,7 +244,7 @@ def test_filter_cases_returns_cases_only_where_criteria_is_met():
             "qDataBag.TelNo2": "",
             "TelNoAppt": "",
             "qDataBag.Wave": "1",
-            "qDataBag.Priority": "1", 
+            "qDataBag.Priority": "1",
             "hOut": 0
         },
         {
@@ -253,7 +253,7 @@ def test_filter_cases_returns_cases_only_where_criteria_is_met():
             "qDataBag.TelNo2": "",
             "TelNoAppt": "",
             "qDataBag.Wave": "1",
-            "qDataBag.Priority": "2", 
+            "qDataBag.Priority": "2",
             "hOut": 0
         },
         {
@@ -262,7 +262,7 @@ def test_filter_cases_returns_cases_only_where_criteria_is_met():
             "qDataBag.TelNo2": "",
             "TelNoAppt": "",
             "qDataBag.Wave": "1",
-            "qDataBag.Priority": "3", 
+            "qDataBag.Priority": "3",
             "hOut": 0
         },
         {
@@ -271,7 +271,7 @@ def test_filter_cases_returns_cases_only_where_criteria_is_met():
             "qDataBag.TelNo2": "",
             "TelNoAppt": "",
             "qDataBag.Wave": "1",
-            "qDataBag.Priority": "4", 
+            "qDataBag.Priority": "4",
             "hOut": 0
         },
         {
@@ -280,21 +280,27 @@ def test_filter_cases_returns_cases_only_where_criteria_is_met():
             "qDataBag.TelNo2": "",
             "TelNoAppt": "",
             "qDataBag.Wave": "1",
-            "qDataBag.Priority": "5", 
+            "qDataBag.Priority": "5",
             "hOut": 0
         },
     ]
-    
+
     # act
     result = filter_cases(cases)
 
     # assert
-    assert result == [{"qDataBag.TelNo": "","qDataBag.TelNo2": "","TelNoAppt": "","qDataBag.Wave": "1","qDataBag.Priority": "1", "hOut": 310 },
-                        {"qDataBag.TelNo": "","qDataBag.TelNo2": "","TelNoAppt": "","qDataBag.Wave": "1","qDataBag.Priority": "1", "hOut": 0 },
-                        {"qDataBag.TelNo": "","qDataBag.TelNo2": "","TelNoAppt": "","qDataBag.Wave": "1","qDataBag.Priority": "2", "hOut": 0 },
-                        {"qDataBag.TelNo": "","qDataBag.TelNo2": "","TelNoAppt": "","qDataBag.Wave": "1","qDataBag.Priority": "3", "hOut": 0 },
-                        {"qDataBag.TelNo": "","qDataBag.TelNo2": "","TelNoAppt": "","qDataBag.Wave": "1","qDataBag.Priority": "4", "hOut": 0 },
-                        {"qDataBag.TelNo": "","qDataBag.TelNo2": "","TelNoAppt": "","qDataBag.Wave": "1","qDataBag.Priority": "5", "hOut": 0 }]
+    assert result == [{"qDataBag.TelNo": "", "qDataBag.TelNo2": "", "qDataBag.TelNoAppt": "", "qDataBag.Wave": "1",
+                       "qDataBag.Priority": "1", "hOut": 310},
+                      {"qDataBag.TelNo": "", "qDataBag.TelNo2": "", "qDataBag.TelNoAppt": "", "qDataBag.Wave": "1",
+                       "qDataBag.Priority": "1", "hOut": 0},
+                      {"qDataBag.TelNo": "", "qDataBag.TelNo2": "", "qDataBag.TelNoAppt": "", "qDataBag.Wave": "1",
+                       "qDataBag.Priority": "2", "hOut": 0},
+                      {"qDataBag.TelNo": "", "qDataBag.TelNo2": "", "qDataBag.TelNoAppt": "", "qDataBag.Wave": "1",
+                       "qDataBag.Priority": "3", "hOut": 0},
+                      {"qDataBag.TelNo": "", "qDataBag.TelNo2": "", "qDataBag.TelNoAppt": "", "qDataBag.Wave": "1",
+                       "qDataBag.Priority": "4", "hOut": 0},
+                      {"qDataBag.TelNo": "", "qDataBag.TelNo2": "", "qDataBag.TelNoAppt": "", "qDataBag.Wave": "1",
+                       "qDataBag.Priority": "5", "hOut": 0}]
 
 
 def test_validate_request(mock_create_job_task):
@@ -305,7 +311,7 @@ def test_validate_request_missing_fields():
     with pytest.raises(Exception) as err:
         validate_request({"world_id": ""})
     assert (
-        str(err.value) == "Required fields missing from request payload: ['questionnaire']"
+            str(err.value) == "Required fields missing from request payload: ['questionnaire']"
     )
 
 
@@ -314,25 +320,25 @@ def test_validate_request_missing_fields():
 @mock.patch("cloud_functions.create_questionnaire_case_tasks.filter_cases")
 @mock.patch("cloud_functions.create_questionnaire_case_tasks.run_async_tasks")
 def test_create_case_tasks_for_questionnaire(
-    mock_run_async_tasks,
-    mock_filter_cases,
-    mock_retrieve_case_data,
-    mock_retrieve_world_id,
+        mock_run_async_tasks,
+        mock_filter_cases,
+        mock_retrieve_case_data,
+        mock_retrieve_world_id,
 ):
     # arrange
     mock_request = flask.Request.from_values(json={"questionnaire": "LMS2101_AA1"})
-    config = Config("", "", "", "", "queue-id", "cloud-function", "", "", "", "", "",)
-    mock_retrieve_case_data.return_value = [{"qiD.Serial_Number": "10010"},{"qiD.Serial_Number": "10012"}]
+    config = Config("", "", "", "", "queue-id", "cloud-function", "", "", "", "", "", )
+    mock_retrieve_case_data.return_value = [{"qiD.Serial_Number": "10010"}, {"qiD.Serial_Number": "10012"}]
     mock_retrieve_world_id.return_value = "1"
     mock_filter_cases.return_value = [{"qiD.Serial_Number": "10010"}]
-    
+
     # act
     result = create_questionnaire_case_tasks(mock_request, config)
 
     # assert
     mock_retrieve_case_data.assert_called_with("LMS2101_AA1", config)
     mock_retrieve_world_id.assert_called_with(config)
-    mock_filter_cases.assert_called_with([{"qiD.Serial_Number": "10010"},{"qiD.Serial_Number": "10012"}])
+    mock_filter_cases.assert_called_with([{"qiD.Serial_Number": "10010"}, {"qiD.Serial_Number": "10012"}])
     mock_run_async_tasks.assert_called_once()
     kwargs = mock_run_async_tasks.call_args.kwargs
     assert kwargs['cloud_function_name'] == "cloud-function"
@@ -340,20 +346,20 @@ def test_create_case_tasks_for_questionnaire(
     assert len(kwargs['tasks']) == 1
     task = kwargs['tasks'][0]
     assert task[0][0:3] == "LMS"
-    assert json.loads(task[1]) == {'case': {'qiD.Serial_Number': '10010'}, 
-                                'questionnaire': 'LMS2101_AA1', 'world_id': '1'}
+    assert json.loads(task[1]) == {'case': {'qiD.Serial_Number': '10010'},
+                                   'questionnaire': 'LMS2101_AA1', 'world_id': '1'}
     assert result == "Done"
 
 
 def test_create_questionnaire_case_tasks_error():
     # arrange
     mock_request = flask.Request.from_values(json={"instrument": ""})
-    config = Config("", "", "", "", "queue-id", "cloud-function", "", "", "", "", "",)
+    config = Config("", "", "", "", "queue-id", "cloud-function", "", "", "", "", "", )
     # assert
     with pytest.raises(Exception) as err:
         create_questionnaire_case_tasks(mock_request, config)
     assert (
-        str(err.value) == "Required fields missing from request payload: ['questionnaire']"
+            str(err.value) == "Required fields missing from request payload: ['questionnaire']"
     )
 
 
@@ -361,17 +367,17 @@ def test_create_questionnaire_case_tasks_error():
 @mock.patch("cloud_functions.create_questionnaire_case_tasks.retrieve_case_data")
 @mock.patch("cloud_functions.create_questionnaire_case_tasks.filter_cases")
 def test_get_wave_from_questionnaire_name_none_LMS_error(
-    mock_filter_cases,
-    mock_retrieve_case_data,
-    mock_retrieve_world_id,
+        mock_filter_cases,
+        mock_retrieve_case_data,
+        mock_retrieve_world_id,
 ):
     # arrange
-    config = Config("", "", "", "", "queue-id", "cloud-function", "", "", "", "", "",)
+    config = Config("", "", "", "", "queue-id", "cloud-function", "", "", "", "", "", )
     mock_request = flask.Request.from_values(json={"questionnaire": "OPN2101A"})
-    mock_retrieve_case_data.return_value = [{"qiD.Serial_Number": "10010"},{"qiD.Serial_Number": "10012"}]
+    mock_retrieve_case_data.return_value = [{"qiD.Serial_Number": "10010"}, {"qiD.Serial_Number": "10012"}]
     mock_retrieve_world_id.return_value = "1"
     mock_filter_cases.return_value = [{"qiD.Serial_Number": "10010"}]
-    
+
     # act
     with pytest.raises(Exception) as err:
         create_questionnaire_case_tasks(mock_request, config)
@@ -384,17 +390,17 @@ def test_get_wave_from_questionnaire_name_none_LMS_error(
 @mock.patch("cloud_functions.create_questionnaire_case_tasks.retrieve_case_data")
 @mock.patch("cloud_functions.create_questionnaire_case_tasks.filter_cases")
 def test_get_wave_from_questionnaire_name_unsupported_wave_error(
-    mock_filter_cases,
-    mock_retrieve_case_data,
-    mock_retrieve_world_id,
+        mock_filter_cases,
+        mock_retrieve_case_data,
+        mock_retrieve_world_id,
 ):
     # arrange
-    config = Config("", "", "", "", "queue-id", "cloud-function", "", "", "", "", "",)
+    config = Config("", "", "", "", "queue-id", "cloud-function", "", "", "", "", "", )
     mock_request = flask.Request.from_values(json={"questionnaire": "LMS2101_AA2"})
-    mock_retrieve_case_data.return_value = [{"qiD.Serial_Number": "10010"},{"qiD.Serial_Number": "10012"}]
+    mock_retrieve_case_data.return_value = [{"qiD.Serial_Number": "10010"}, {"qiD.Serial_Number": "10012"}]
     mock_retrieve_world_id.return_value = "1"
     mock_filter_cases.return_value = [{"qiD.Serial_Number": "10010"}]
-    
+
     # act
     with pytest.raises(Exception) as err:
         create_questionnaire_case_tasks(mock_request, config)
@@ -412,3 +418,65 @@ def test_get_wave_from_questionnaire_name_with_invalid_format_raises_error():
     with pytest.raises(Exception) as err:
         get_wave_from_questionnaire_name("ABC1234_AA1")
     assert str(err.value) == "Invalid format for questionnaire name: ABC1234_AA1"
+
+
+@mock.patch.object(OptimiseClient, "get_worlds")
+def test_retrieve_world_id_correctly_maps_world_id_to_world(_mock_optimise_client):
+    # arrange
+    config = Config(
+        "totalmobile_url",
+        "totalmobile_instance",
+        "totalmobile_client_id",
+        "totalmobile_client_secret",
+        "",
+        "",
+        "",
+        "",
+        "rest_api_url",
+        "gusty",
+        "",
+    )
+
+    filtered_cases = [
+        {"qDataBag.FieldRegion": "Region 1"},
+        {"qDataBag.FieldRegion": "Region 2"},
+        {"qDataBag.FieldRegion": "Region 4"},
+    ]
+
+    _mock_optimise_client.return_value = [
+        {
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "identity": {
+                "reference": "Region 1"
+            },
+            "type": "foo"
+        },
+        {
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa7",
+            "identity": {
+                "reference": "Region 2"
+            },
+            "type": "foo"
+        },
+        {
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa8",
+            "identity": {
+                "reference": "Region 3"
+            },
+            "type": "foo"
+        },
+        {
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa9",
+            "identity": {
+                "reference": "Region 4"
+            },
+            "type": "foo"
+        },
+    ]
+
+    # assert
+    assert retrieve_world_id(config, filtered_cases) == [
+        "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "3fa85f64-5717-4562-b3fc-2c963f66afa7",
+        "3fa85f64-5717-4562-b3fc-2c963f66afa9",
+    ]
