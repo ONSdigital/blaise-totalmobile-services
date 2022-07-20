3bdb6a1d87f8fb89dfa5cafbeaaa6d00c5dbf3ba
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-07-20 12:23:01 +0100

    refactor: change return type for get_worlds method
    Reviewed-by: James
    Refs: BLAIS5-3188

--- client/optimise.py
+++ client/optimise.py
@@ -25,7 +25,7 @@ class OptimiseClient(BaseClient):
             f"worlds/{world_id}/jobs/{job_reference}/additionalProperties"
         ).json()
 
-    def get_worlds(self) -> List[Any]:
+    def get_worlds(self) -> List[Dict[str, str]]:
         return self._get("worlds").json()
 
     def get_world(self, world: str) -> Dict[Any, Any]:
