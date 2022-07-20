65a321bbb557f09938021625181293f286e2928a
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-07-20 16:00:49 +0100

    refactor: call retrieve case id method in unit test
    Accidentally removed this - oops
    
    Reviewed-by: James
    Refs: BLAIS5-3188

--- tests/cloud_functions/test_create_questionnaire_case_tasks.py
+++ tests/cloud_functions/test_create_questionnaire_case_tasks.py
@@ -78,6 +78,9 @@ def test_retrieve_case_data_calls_the_rest_api_client_with_the_correct_parameter
         "qDataBag.FieldRegion"
     ]
 
+    # act
+    retrieve_case_data(questionnaire_name, config)
+
     # assert
     _mock_rest_api_client.assert_called_with(
         blaise_server_park, questionnaire_name, fields
