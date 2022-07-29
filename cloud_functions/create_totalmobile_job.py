2634925777c4e854b6a0bf7b7c4b5a924adc4d4b
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-07-29 09:18:11 +0100

    refactor: remove unwanted logs
    Reviewed-by: James
    Refs: BLAIS5-3182

--- cloud_functions/create_totalmobile_job.py
+++ cloud_functions/create_totalmobile_job.py
@@ -60,8 +60,6 @@ def create_job_payload(request_json: Dict) -> Dict:
     questionnaire = request_json["questionnaire"]
     case = request_json["case"]
 
-    logging.info(f"Questionnaire: {questionnaire}")
-    logging.info(f"Case: {case}")
     totalmobile_payload = {
         "identity": {
             "reference": create_job_reference(questionnaire, case["qiD.Serial_Number"]),
