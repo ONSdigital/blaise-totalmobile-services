471da5fae34484e11643054c1dc3c8aaa984c652
jamie kerr <jamiestephenkerr@gmail.com>
2022-08-01 16:41:21 +0100

    meh

--- models/totalmobile_case_model.py
+++ models/totalmobile_case_model.py
@@ -9,3 +9,7 @@ class AddressModel:
     addressLine1: str
     addressLine2: str
     addressLine3: str    
+
+
+    def populate_questionnaire_case_data(cls, QuestionnaireCaseModel) -> bool:
+        cls.location.addressLine1 = QuestionnaireCaseModel.address_line_1
