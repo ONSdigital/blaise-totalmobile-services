7c49c25714774f05f55525b251329d4bd2430c14
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-08-09 16:05:29 +0100

    refactor: removed commented out code
    Co-authored-by: James Williams <james.williams@ons.gov.uk>
    Refs: BLAIS5-3261

--- cloud_functions/create_totalmobile_job.py
+++ cloud_functions/create_totalmobile_job.py
@@ -10,53 +10,6 @@ from services import totalmobile_service
 setup_logger()
 
 
-# def validate_request(request_json: Dict) -> None:
-#     REQUIRED_FIELDS = ["questionnaire", "case", "world_id"]
-#     missing_fields = __filter_missing_fields(request_json, REQUIRED_FIELDS)
-#     if len(missing_fields) >= 1:
-#         raise Exception(
-#             f"Required fields missing from request payload: {missing_fields}"
-#         )
-#     validate_case_data(request_json["case"])
-
-#
-# def validate_case_data(case: Dict) -> None:
-#     REQUIRED_FIELDS = [
-#         "qiD.Serial_Number",
-#         "dataModelName",
-#         "qDataBag.TLA",
-#         "qDataBag.Wave",
-#         "qDataBag.Prem1",
-#         "qDataBag.Prem2",
-#         "qDataBag.Prem3",
-#         "qDataBag.District",
-#         "qDataBag.PostTown",
-#         "qDataBag.PostCode",
-#         "qDataBag.TelNo",
-#         "qDataBag.TelNo2",
-#         "telNoAppt",
-#         "hOut",
-#         "qDataBag.UPRN_Latitude",
-#         "qDataBag.UPRN_Longitude",
-#         "qDataBag.Priority",
-#         "qDataBag.FieldRegion",
-#         "qDataBag.FieldTeam",
-#         "qDataBag.WaveComDTE",
-#     ]
-#     missing_fields = __filter_missing_fields(case, REQUIRED_FIELDS)
-#     if len(missing_fields) >= 1:
-#         raise Exception(f"Required fields missing from case data: {missing_fields}")
-
-# def validate_totalmobile_payload(totalmobile_payload):
-#     if "duration" not in totalmobile_payload:
-#         logging.warning("Totalmobile payload was sent without the 'duration' field")
-#     if "origin" not in totalmobile_payload:
-#         logging.warning("Totalmobile payload was sent without the 'origin' field")
-#     for property in totalmobile_payload["additionalProperties"]:
-#         if "uac" in property["name"] and property["value"] == "":
-#             logging.warning(f"Totalmobile payload was sent with an empty {property['name']} field")
-
-
 def create_totalmobile_job(request: flask.Request) -> str:
     config = Config.from_env()
     config.validate()
@@ -76,7 +29,3 @@ def create_totalmobile_job(request: flask.Request) -> str:
     response = totalmobile_service.create_job(config, totalmobile_job)
     logging.info(f"Response: {response}")
     return "Done"
-
-
-# def __filter_missing_fields(case, REQUIRED_FIELDS) -> List[str]:
-#     return list(filter(lambda field: field not in case, REQUIRED_FIELDS))
