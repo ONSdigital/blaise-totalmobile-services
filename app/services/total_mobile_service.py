af7835607b5efb21ee7594ff37467ba2c9ee074e
calnic <98323072+cal-nic@users.noreply.github.com>
2022-08-10 14:21:41 +0100

    "add update case field to questionnaire service"

--- app/services/total_mobile_service.py
+++ app/services/total_mobile_service.py
@@ -1,21 +1,12 @@
-import blaise_restapi
-
-from appconfig import Config
-
+from appconfig.config import Config
+from services import questionnaire_service
 
 def update_case_telephone_number(
     questionnaire_name: str, case_id: str, telephone_number: str
 ) -> None:
     print(f"Updating telephone number for {questionnaire_name}, {case_id}, please wait...")
     config = Config.from_env()
-    restapi_client = blaise_restapi.Client(config.blaise_api_url)
-    print(f"\nrest_api: {restapi_client}")
-
-    data_fields = {"qDataBag.TelNo": telephone_number}
-    print(f"\ndata_fields: {data_fields}")
-    restapi_client.patch_case_data(
-        config.blaise_server_park, questionnaire_name, case_id, data_fields
-    )
+    questionnaire_service.update_case_field(questionnaire_name, case_id, "qDataBag.TelNo", telephone_number, config)
 
 
 def do_something_service(reference: str) -> None:
