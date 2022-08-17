9c7c188be00d172309318469d35e69d109bf9e17
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-08-17 19:52:33 +0100

    wip

--- scripts/delete_totalmobile_jobs.py
+++ scripts/delete_totalmobile_jobs.py
@@ -2,16 +2,21 @@ from appconfig import Config
 from services import totalmobile_service
 
 
+def get_list_of_world_ids(config):
+    return totalmobile_service.get_worlds(config).get_available_ids()
+
+
 def delete_all_totalmobile_jobs_in_active_worlds(config: Config) -> str:
-    world_ids = totalmobile_service.get_worlds(config)
+    world_ids = get_list_of_world_ids(config)
 
     for world_id in world_ids:
-        jobs = totalmobile_service.get_jobs(config, world_id["id"])
+        jobs = totalmobile_service.get_jobs(config, world_id)
         for job in jobs:
-            totalmobile_service.delete_job(config, world_id["id"], job["results"]["identity"]["reference"])
+            totalmobile_service.delete_job(config, world_id.id, job["results"][0]["identity"]["reference"])
     return "Done"
 
 
 if __name__ == "__main__":
     config = Config.from_env()
+
     delete_all_totalmobile_jobs_in_active_worlds(config)
--- tests/scripts/test_delete_jobs_in_totalmobile.py
+++ tests/scripts/test_delete_jobs_in_totalmobile.py
@@ -1,22 +1,19 @@
-from delete_jobs_in_totalmobile import delete_all_totalmobile_jobs_in_active_worlds
+from scripts.delete_totalmobile_jobs import delete_all_totalmobile_jobs_in_active_worlds, get_list_of_world_ids
 from unittest import mock
+from models.totalmobile_world_model import TotalmobileWorldModel, World
+from tests.helpers import config_helper
 
 
-@mock.patch("services.totalmobile_service.delete_jobs")
-@mock.patch("services.totalmobile_service.get_jobs")
 @mock.patch("services.totalmobile_service.get_worlds")
-def test_delete_all_totalmobile_jobs_in_active_worlds(
-        mock_get_worlds,
-        mock_get_jobs,
-        mock_delete_jobs,
-):
+def test_get_list_of_world_ids_returns_a_list_of_world_ids(mock_get_worlds):
     mock_get_worlds.return_value = TotalmobileWorldModel(
         worlds=[
             World(region="Region 1", id="3fa85f64-5717-4562-b3fc-2c963f66afa6"),
-            World(region="Region 2", id="3fa85f64-5717-4562-b3fc-2c963f66afa7")
+            World(region="Region 2", id="3fa85f64-5717-4562-b3fc-2c963f66afa6")
         ],
     )
-
-    mock_get_jobs.return_value = {
-
-    }
+    config = config_helper.get_default_config()
+    assert get_list_of_world_ids(config) == [
+        "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    ]
