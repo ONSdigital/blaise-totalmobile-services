c5a19cab1ae36fb6d354bcea6c66fbaae575f530
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-08-17 16:06:17 +0100

    refactor: add getting config

--- scripts/delete_jobs_in_totalmobile.py
+++ scripts/delete_jobs_in_totalmobile.py
@@ -2,10 +2,16 @@ from appconfig import Config
 from services import totalmobile_service
 
 
-if __name__ == "__main__":
+def delete_all_totalmobile_jobs_in_active_worlds(config: Config) -> str:
     world_ids = totalmobile_service.get_worlds(config)
 
     for world_id in world_ids:
         jobs = totalmobile_service.get_jobs(config, world_id["id"])
         for job in jobs:
             totalmobile_service.delete_job(config, world_id["id"], job["results"]["identity"]["reference"])
+    return "Done"
+
+
+if __name__ == "__main__":
+    config = Config.from_env()
+    delete_all_totalmobile_jobs_in_active_worlds(config)
