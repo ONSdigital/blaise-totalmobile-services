b2670038c55388a2955a49e9231209db3b0db212
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-08-18 11:46:03 +0100

    wip

--- scripts/delete_totalmobile_jobs.py
+++ scripts/delete_totalmobile_jobs.py
@@ -3,13 +3,16 @@ from services import totalmobile_service
 from typing import List
 
 def get_list_of_active_world_ids(config):
+    print("Retrieving world ids")
     return totalmobile_service.get_worlds(config).get_available_ids()
 
 
 def delete_all_totalmobile_jobs_in_active_worlds(config: Config, world_ids: List[str]) -> str:
     for world_id in world_ids:
+        print(f"Retrieving jobs in world {world_id}")
         jobs = totalmobile_service.get_jobs(config, world_id)
         for job in jobs:
+            print(f"Deleting job {job['results'][0]['identity']['reference']} in {world_id}")
             totalmobile_service.delete_job(config, world_id, job["results"][0]["identity"]["reference"])
     return "Done"
 
