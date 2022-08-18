5846a0b89dc48719d0470e97c2fb168c6b4eed47
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-08-18 11:25:04 +0100

    wip

--- tests/scripts/test_delete_jobs_in_totalmobile.py
+++ tests/scripts/test_delete_jobs_in_totalmobile.py
@@ -24,13 +24,15 @@ def test_get_list_of_active_world_ids_returns_a_list_of_active_world_ids(mock_ge
 def test_delete_all_totalmobile_jobs_in_active_world_deletes_all_jobs(mock_get_jobs, mock_delete_job):
     config = config_helper.get_default_config()
     list_of_active_world_ids = ["3fa85f64-5717-4562-b3fc-2c963f66afa6"]
+
     mock_get_jobs.return_value = [
         {"results": [{"identity": {"reference": "Foo"}}]},
         {"results": [{"identity": {"reference": "Bar"}}]}
     ]
-    mock_delete_job.return_value = []
-    result = delete_all_totalmobile_jobs_in_active_worlds(config, list_of_active_world_ids)
-    mock_get_jobs.assert_called_with(config, "3fa85f64-5717-4562-b3fc-2c963f66afa6")
+    mock_delete_job.return_value.status_code = 204
 
+    result = delete_all_totalmobile_jobs_in_active_worlds(config, list_of_active_world_ids)
+
+    mock_get_jobs.assert_called_with(config, "3fa85f64-5717-4562-b3fc-2c963f66afa6")
     mock_delete_job.assert_called_with(config, "3fa85f64-5717-4562-b3fc-2c963f66afa6", "Bar")
     assert result == "Done"
