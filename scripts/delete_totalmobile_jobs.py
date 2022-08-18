0828b8972ebee16113b51a8f1166d02e7ba369d3
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-08-18 08:35:18 +0100

    wip

--- scripts/delete_totalmobile_jobs.py
+++ scripts/delete_totalmobile_jobs.py
@@ -2,13 +2,11 @@ from appconfig import Config
 from services import totalmobile_service
 
 
-def get_list_of_world_ids(config):
+def get_list_of_active_world_ids(config):
     return totalmobile_service.get_worlds(config).get_available_ids()
 
 
-def delete_all_totalmobile_jobs_in_active_worlds(config: Config) -> str:
-    world_ids = get_list_of_world_ids(config)
-
+def delete_all_totalmobile_jobs_in_active_worlds(config: Config, world_ids: List[str]) -> str:
     for world_id in world_ids:
         jobs = totalmobile_service.get_jobs(config, world_id)
         for job in jobs:
@@ -19,4 +17,5 @@ def delete_all_totalmobile_jobs_in_active_worlds(config: Config) -> str:
 if __name__ == "__main__":
     config = Config.from_env()
 
-    delete_all_totalmobile_jobs_in_active_worlds(config)
+    list_of_active_world_ids = get_list_of_active_world_ids(config)
+    delete_all_totalmobile_jobs_in_active_worlds(config, list_of_active_world_ids)
--- tests/scripts/test_delete_jobs_in_totalmobile.py
+++ tests/scripts/test_delete_jobs_in_totalmobile.py
@@ -1,11 +1,11 @@
-from scripts.delete_totalmobile_jobs import delete_all_totalmobile_jobs_in_active_worlds, get_list_of_world_ids
+from scripts.delete_totalmobile_jobs import delete_all_totalmobile_jobs_in_active_worlds, get_list_of_active_world_ids
 from unittest import mock
 from models.totalmobile_world_model import TotalmobileWorldModel, World
 from tests.helpers import config_helper
 
 
 @mock.patch("services.totalmobile_service.get_worlds")
-def test_get_list_of_world_ids_returns_a_list_of_world_ids(mock_get_worlds):
+def test_get_list_of_active_world_ids_returns_a_list_of_active_world_ids(mock_get_worlds):
     mock_get_worlds.return_value = TotalmobileWorldModel(
         worlds=[
             World(region="Region 1", id="3fa85f64-5717-4562-b3fc-2c963f66afa6"),
@@ -13,7 +13,7 @@ def test_get_list_of_world_ids_returns_a_list_of_world_ids(mock_get_worlds):
         ],
     )
     config = config_helper.get_default_config()
-    assert get_list_of_world_ids(config) == [
+    assert get_list_of_active_world_ids(config) == [
         "3fa85f64-5717-4562-b3fc-2c963f66afa6",
         "3fa85f64-5717-4562-b3fc-2c963f66afa6",
     ]
