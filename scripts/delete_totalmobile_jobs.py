7e8d30844ac0ed55bbd00a408999b8aecedb4ed5
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-08-19 11:28:14 +0100

    refactor: break out into individual functions that can be tested
    refs: BLAIS5-3303

--- scripts/delete_totalmobile_jobs.py
+++ scripts/delete_totalmobile_jobs.py
@@ -1,34 +1,54 @@
 import asyncio
 from appconfig import Config
 from services import totalmobile_service
-from typing import List
+from typing import List, Dict
 
-def get_list_of_active_world_ids(config):
+
+def remove_default_world_id_from_the_list_of_active_world_ids(list_of_active_world_ids: List[str]) -> List[str]:
+    print("Removing the default world id from retrieved world ids")
+    default_world_id = "c0ffee00-c8d0-499f-8693-8be6ad1dc6ea"
+    return [world_id for world_id in list_of_active_world_ids if world_id != default_world_id]
+
+
+def get_list_of_active_world_ids(config: Config) -> List[str]:
     print("Retrieving world ids")
-    return totalmobile_service.get_worlds(config).get_available_ids()
+    return remove_default_world_id_from_the_list_of_active_world_ids(
+        totalmobile_service.get_worlds(config).get_available_ids()
+    )
 
 
-def delete_all_totalmobile_jobs_in_active_worlds(config: Config, world_ids: List[str]) -> str:
+def get_list_of_world_ids_and_job_references(config: Config, world_ids: List[str]) -> List[Dict[str, str]]:
+    list_of_jobs = []
     for world_id in world_ids:
-        if world_id != "c0ffee00-c8d0-499f-8693-8be6ad1dc6ea":
-            print(f"Getting jobs for world id {world_id}")
-            jobs = totalmobile_service.get_jobs(config, world_id)
-            print(f"Found {len(jobs)} jobs for world id {world_id}")
-            for job in jobs:
-                try:
-                    job_ref = job["identity"]["reference"]
-                    print(f"Deleting job id {job_ref}")
-                    asyncio.run(totalmobile_service.delete_job(config, world_id, job_ref)) # luls
-                except:
-                    print(f"Error deleting job id {job_ref}")                
-    return "Done"
+        print(f"Retrieving job references for {world_id}")
+        jobs = totalmobile_service.get_jobs(config, world_id)
+        for job in jobs:
+            my_dict = {"world_id": world_id, "reference": job["identity"]["reference"]}
+            list_of_jobs.append(my_dict)
+    print(f"Found a total of {len(list_of_jobs)} jobs across {len(world_ids)} worlds")
+    return list_of_jobs
+
+
+def delete_job(config: Config, list_of_jobs: List[Dict[str, str]]) -> str:
+    for job in list_of_jobs:
+        try:
+            print(f"Deleting job id {job['reference']} from world {job['world_id']}")
+            totalmobile_service.delete_job(config, job["world_id"], job["reference"])
+        except:
+            print(f"Error deleting job id {job['reference']} from world {job['world_id']}")
+    return "Done!"
 
 
 if __name__ == "__main__":
     config = Config.from_env()
     print(config)
+
     if config.totalmobile_url.lower().__contains__("dev"):
         list_of_active_world_ids = get_list_of_active_world_ids(config)
-        delete_all_totalmobile_jobs_in_active_worlds(config, list_of_active_world_ids)
+        list_of_jobs = get_list_of_world_ids_and_job_references(config, list_of_active_world_ids)
+
+        # TODO: Get asyncio working
+        asyncio.run(delete_totalmobile_job(config, list_of_jobs))
+
     else:
-        print("no m8")
+        print("Totalmobile URL was not pointing at 'dev'")
--- tests/scripts/test_delete_jobs_in_totalmobile.py
+++ tests/scripts/test_delete_jobs_in_totalmobile.py
@@ -1,4 +1,9 @@
-from scripts.delete_totalmobile_jobs import delete_all_totalmobile_jobs_in_active_worlds, get_list_of_active_world_ids
+from scripts.delete_totalmobile_jobs import (
+    get_list_of_active_world_ids,
+    remove_default_world_id_from_the_list_of_active_world_ids,
+    get_list_of_world_ids_and_job_references,
+    delete_job,
+)
 from unittest import mock
 from models.totalmobile_world_model import TotalmobileWorldModel, World
 from tests.helpers import config_helper
@@ -13,26 +18,87 @@ def test_get_list_of_active_world_ids_returns_a_list_of_active_world_ids(mock_ge
         ],
     )
     config = config_helper.get_default_config()
+
     assert get_list_of_active_world_ids(config) == [
         "3fa85f64-5717-4562-b3fc-2c963f66afa6",
         "3fa85f64-5717-4562-b3fc-2c963f66afa6",
     ]
 
 
-@mock.patch("services.totalmobile_service.delete_job")
-@mock.patch("services.totalmobile_service.get_jobs")
-def test_delete_all_totalmobile_jobs_in_active_world_deletes_all_jobs(mock_get_jobs, mock_delete_job):
+def test_remove_default_world_returns_a_list_without_default_world():
+    default_world_id = "c0ffee00-c8d0-499f-8693-8be6ad1dc6ea"
+    list_of_active_world_ids = [
+        default_world_id,
+        "c0ffee00-c8d0-499f-8693-8be6ad1dc7ea",
+        "c0ffee00-c8d0-499f-8693-8be6ad1dc8ea",
+        "c0ffee00-c8d0-499f-8693-8be6ad1dc9ea",
+    ]
+    assert remove_default_world_id_from_the_list_of_active_world_ids(list_of_active_world_ids) == [
+        "c0ffee00-c8d0-499f-8693-8be6ad1dc7ea",
+        "c0ffee00-c8d0-499f-8693-8be6ad1dc8ea",
+        "c0ffee00-c8d0-499f-8693-8be6ad1dc9ea",
+    ]
+
+
+@mock.patch("services.totalmobile_service.get_worlds")
+def test_get_list_of_active_world_ids_returns_a_list_of_active_world_ids_when_default_world_id_is_returned(mock_get_worlds):
+    default_world_id = "c0ffee00-c8d0-499f-8693-8be6ad1dc6ea"
+    mock_get_worlds.return_value = TotalmobileWorldModel(
+        worlds=[
+            World(region="Region 1", id="3fa85f64-5717-4562-b3fc-2c963f66afa6"),
+            World(region="Region 2", id="3fa85f64-5717-4562-b3fc-2c963f66afa6"),
+            World(region="Region 3", id=default_world_id),
+        ],
+    )
     config = config_helper.get_default_config()
-    list_of_active_world_ids = ["3fa85f64-5717-4562-b3fc-2c963f66afa6"]
+
+    assert get_list_of_active_world_ids(config) == [
+        "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    ]
+
+
+@mock.patch("services.totalmobile_service.get_jobs")
+def test_build_dictionary(mock_get_jobs):
+    config = config_helper.get_default_config()
+    list_of_active_world_ids = ["3fa85f64-5717-4562-b3fc-2c963f66afa6", "3fa85f64-5717-4562-b3fc-2c963f66afa7"]
 
     mock_get_jobs.return_value = [
-        {"results": [{"identity": {"reference": "Foo"}}]},
-        {"results": [{"identity": {"reference": "Bar"}}]}
+        {"identity": {"reference": "Foo"}},
+        {"identity": {"reference": "Bar"}},
     ]
-    mock_delete_job.return_value.status_code = 204
 
-    result = delete_all_totalmobile_jobs_in_active_worlds(config, list_of_active_world_ids)
+    assert get_list_of_world_ids_and_job_references(config, list_of_active_world_ids) == [
+        {
+            "world_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "reference": "Foo",
+        },
+        {
+            "world_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "reference": "Bar",
+        },
+        {
+            "world_id": "3fa85f64-5717-4562-b3fc-2c963f66afa7",
+            "reference": "Foo",
+        },
+        {
+            "world_id": "3fa85f64-5717-4562-b3fc-2c963f66afa7",
+            "reference": "Bar",
+        },
+    ]
 
-    mock_get_jobs.assert_called_with(config, "3fa85f64-5717-4562-b3fc-2c963f66afa6")
+
+@mock.patch("services.totalmobile_service.delete_job")
+def test_delete_job(mock_delete_job):
+    config = config_helper.get_default_config()
+    list_of_jobs = [
+        {
+            "world_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "reference": "Bar",
+        },
+    ]
+
+    result = delete_job(config, list_of_jobs)
+
     mock_delete_job.assert_called_with(config, "3fa85f64-5717-4562-b3fc-2c963f66afa6", "Bar")
-    assert result == "Done"
+    assert result == "Done!"
