03611d5c82f634bad65c0ddd517e3121f2784768
Tom Oram <tom.oram@armakuni.com>
2022-09-05 14:39:30 +0100

    refactor: Inject blaise_service into QuestionnaireService
    Refs: BLAIS5-3383

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from app.auth import auth
 from app.config import Config
 from app.endpoints import incoming
+from services import blaise_service
 from services.questionnaire_service import QuestionnaireService
 
 
@@ -17,5 +18,7 @@ def setup_app():
     application.auth = auth
     application.register_blueprint(incoming)
     config = Config.from_env()
-    application.questionnaire_service = QuestionnaireService(config)
+    application.questionnaire_service = QuestionnaireService(
+        config, blaise_service=blaise_service
+    )
     return application
