40a9540d2bd506dae8e078196cc6ceb7d25a8890
Tom Oram <tom.oram@armakuni.com>
2022-09-05 17:08:54 +0100

    fix: Fix some broken calls caught by Behave tests
    Refs: BLAIS5-3383

--- services/update_case_service.py
+++ services/update_case_service.py
@@ -10,6 +10,7 @@ from models.blaise.blaise_case_update_model import BlaiseCaseUpdateModel
 from models.totalmobile.totalmobile_incoming_update_request_model import (
     TotalMobileIncomingUpdateRequestModel,
 )
+from services.questionnaire_service import QuestionnaireService
 
 
 def update_case(
@@ -18,13 +19,12 @@ def update_case(
     questionnaire_service,
 ) -> None:
     validate_questionnaire_exists(
-        totalmobile_request.questionnaire_name, config, questionnaire_service
+        totalmobile_request.questionnaire_name, questionnaire_service
     )
 
     blaise_case = get_case(
         totalmobile_request.questionnaire_name,
         totalmobile_request.case_id,
-        config,
         questionnaire_service,
     )
     update_blaise_case_model = BlaiseCaseUpdateModel.import_case(totalmobile_request)
@@ -35,13 +35,13 @@ def update_case(
         320,
     ):
         _update_case_contact_information(
-            blaise_case, update_blaise_case_model, config, questionnaire_service
+            blaise_case, update_blaise_case_model, questionnaire_service
         )
         return
 
     if totalmobile_request.outcome_code in (460, 461, 510, 540, 551, 560, 580, 640):
         _update_case_outcome_code(
-            blaise_case, update_blaise_case_model, config, questionnaire_service
+            blaise_case, update_blaise_case_model, questionnaire_service
         )
         return
 
@@ -53,7 +53,7 @@ def update_case(
 
 
 def _update_case_contact_information(
-    blaise_case, update_blaise_case_model, config, questionnaire_service
+    blaise_case, update_blaise_case_model, questionnaire_service: QuestionnaireService
 ) -> None:
     fields_to_update = {}
 
@@ -70,7 +70,7 @@ def _update_case_contact_information(
     fields_to_update.update(update_blaise_case_model.knock_to_nudge_indicator_flag())
 
     questionnaire_service.update_case(
-        blaise_case.questionnaire_name, blaise_case.case_id, fields_to_update, config
+        blaise_case.questionnaire_name, blaise_case.case_id, fields_to_update
     )
 
     logging.info(
@@ -81,7 +81,7 @@ def _update_case_contact_information(
 
 
 def _update_case_outcome_code(
-    blaise_case, update_blaise_case_model, config, questionnaire_service
+    blaise_case, update_blaise_case_model, questionnaire_service: QuestionnaireService
 ) -> None:
     fields_to_update = {}
     fields_to_update.update(update_blaise_case_model.outcome_details())
@@ -92,7 +92,7 @@ def _update_case_outcome_code(
         fields_to_update.update(update_blaise_case_model.call_history_record(5))
 
     questionnaire_service.update_case(
-        blaise_case.questionnaire_name, blaise_case.case_id, fields_to_update, config
+        blaise_case.questionnaire_name, blaise_case.case_id, fields_to_update
     )
 
     logging.info(
@@ -103,9 +103,9 @@ def _update_case_outcome_code(
 
 
 def validate_questionnaire_exists(
-    questionnaire_name: str, config: Config, questionnaire_service
+    questionnaire_name: str, questionnaire_service: QuestionnaireService
 ) -> None:
-    if not questionnaire_service.questionnaire_exists(questionnaire_name, config):
+    if not questionnaire_service.questionnaire_exists(questionnaire_name):
         logging.error(f"Could not find questionnaire {questionnaire_name} in Blaise")
         raise QuestionnaireDoesNotExistError()
 
@@ -113,10 +113,10 @@ def validate_questionnaire_exists(
 
 
 def get_case(
-    questionnaire_name: str, case_id: str, config: Config, questionnaire_service
+    questionnaire_name: str, case_id: str, questionnaire_service: QuestionnaireService
 ) -> BlaiseCaseInformationModel:
     try:
-        case = questionnaire_service.get_case(questionnaire_name, case_id, config)
+        case = questionnaire_service.get_case(questionnaire_name, case_id)
     except QuestionnaireCaseDoesNotExistError as err:
         logging.error(
             f"Could not find case {case_id} for questionnaire {questionnaire_name} in Blaise"
