078137c2ff76f6cf31f7c83e3d25a5ba404b0b89
elthorne <46898565+elthorne@users.noreply.github.com>
2022-09-08 15:45:49 +0100

    WIP

--- services/delete_totalmobile_jobs_service.py
+++ services/delete_totalmobile_jobs_service.py
@@ -1,6 +1,7 @@
 from models.totalmobile.totalmobile_reference_model import TotalmobileReferenceModel
 from services.blaise_service import BlaiseService
 from services.totalmobile_service import TotalmobileService
+from typing import Dict, List
 
 
 class DeleteTotalmobileJobsService:
@@ -8,18 +9,31 @@ class DeleteTotalmobileJobsService:
         self.totalmobile_service = totalmobile_service
         self.blaise_service = blaise_service
 
+    def map_shizznizz(self, totalmobile_reference_models: list[TotalmobileReferenceModel]) -> Dict[str, List[str]]:
+        x = {}
+        for model in totalmobile_reference_models:
+            if model.questionnaire_name in x.keys():
+                x[model.questionnaire_name].append(model.case_id)
+            else:
+                x[model.questionnaire_name] = [model.case_id]
+
+        return x
+
     def delete_totalmobile_jobs_completed_in_blaise(self) -> None:
-        job_references = self.get_incomplete_totalmobile_job_references("13013122-d69f-4d6b-gu1d-721f190c4479")
-        totalmobile_reference_models = self.map_reference_models_from_list_of_job_references(job_references)
+        totalmobile_job_references = self.get_incomplete_totalmobile_job_references("13013122-d69f-4d6b-gu1d-721f190c4479")
+        totalmobile_reference_models = self.map_reference_models_from_list_of_job_references(totalmobile_job_references)
 
-        questionnaire_name = totalmobile_reference_models[0].questionnaire_name
+        tm_dictionary_of_questionnaires_with_cases = self.map_shizznizz(totalmobile_reference_models)
 
-        completed_case_ids = self.get_completed_blaise_cases(questionnaire_name)
+        for questionnaire_with_cases_key in tm_dictionary_of_questionnaires_with_cases.keys():
+            completed_case_ids = self.get_completed_blaise_cases(questionnaire_with_cases_key)
 
-        if totalmobile_reference_models[0].case_id in completed_case_ids:
-            self.totalmobile_service.delete_job(
-                "13013122-d69f-4d6b-gu1d-721f190c4479", totalmobile_reference_models[0].create_reference(), "110"
-            )
+            for case_id in tm_dictionary_of_questionnaires_with_cases[questionnaire_with_cases_key]:
+                if case_id in completed_case_ids:
+                    reference = f"{questionnaire_with_cases_key.replace('_', '-')}.{case_id}"
+                    self.totalmobile_service.delete_job(
+                        "13013122-d69f-4d6b-gu1d-721f190c4479", reference, "110"
+                    )
 
     def delete_totalmobile_job(self, world_id: str, job_reference: str, reason: str) -> None:
         self.totalmobile_service.delete_job(world_id, job_reference, reason)
@@ -37,6 +51,6 @@ class DeleteTotalmobileJobsService:
 
         return reference_models
 
-    def get_completed_blaise_cases(self, questionnaire_name):
+    def get_completed_blaise_cases(self, questionnaire_name) -> list[str]:
         case_status_list = self.blaise_service.get_case_status_information(questionnaire_name)
         return [case["primaryKey"] for case in case_status_list if case["outcome"] == 110]
--- tests/services/test_delete_totalmobile_jobs_service.py
+++ tests/services/test_delete_totalmobile_jobs_service.py
@@ -1,6 +1,6 @@
 from services.blaise_service import BlaiseService
 from services.delete_totalmobile_jobs_service import DeleteTotalmobileJobsService
-from unittest.mock import create_autospec
+from unittest.mock import create_autospec, call
 
 from services.totalmobile_service import TotalmobileService
 
@@ -168,36 +168,35 @@ def test_delete_totalmobile_jobs_completed_in_blaise_deletes_jobs_for_completed_
             "identity": {"reference": "LMS1111-AA1.67890"}
         },
     ]
-    # return this thing first ("LMS1111-AA1.67890")
-    mock_blaise_service.get_case_status_information.return_value = [
-        {
-            "primaryKey": "12345",
-            "outcome": 310
-        },
-        {
-            "primaryKey": "22222",
-            "outcome": 310
-        },
-        {
-            "primaryKey": "67890",
-            "outcome": 110
-        }
-    ]
-
-    # return this thing second ("LMS2222-BB2.22222")
-    mock_blaise_service.get_case_status_information.return_value = [
-        {
-            "primaryKey": "12345",
-            "outcome": 310
-        },
-        {
-            "primaryKey": "22222",
-            "outcome": 110
-        },
-        {
-            "primaryKey": "67890",
-            "outcome": 310
-        }
+    mock_blaise_service.get_case_status_information.side_effect = [
+        [
+            {
+                "primaryKey": "12345",
+                "outcome": 310
+            },
+            {
+                "primaryKey": "22222",
+                "outcome": 110
+            },
+            {
+                "primaryKey": "67890",
+                "outcome": 310
+            }
+        ],
+        [
+            {
+                "primaryKey": "12345",
+                "outcome": 310
+            },
+            {
+                "primaryKey": "22222",
+                "outcome": 310
+            },
+            {
+                "primaryKey": "67890",
+                "outcome": 110
+            }
+        ]
     ]
 
     delete_totalmobile_jobs_service = DeleteTotalmobileJobsService(mock_totalmobile_service, mock_blaise_service)
@@ -206,5 +205,78 @@ def test_delete_totalmobile_jobs_completed_in_blaise_deletes_jobs_for_completed_
     delete_totalmobile_jobs_service.delete_totalmobile_jobs_completed_in_blaise()
 
     # assert
-    mock_totalmobile_service.delete_job.assert_called_with(world_id, "LMS1111-AA1.67890", "110")
-    mock_totalmobile_service.delete_job.assert_called_with(world_id, "LMS2222-BB2.22222", "110")
+    mock_totalmobile_service.delete_job.assert_any_call(world_id, "LMS2222-BB2.22222", "110")
+    mock_totalmobile_service.delete_job.assert_any_call(world_id, "LMS1111-AA1.67890", "110")
+
+
+def test_delete_totalmobile_jobs_completed_in_blaise_only_calls_case_status_information_once_per_questionnaire():
+    # arrange
+    world_id = "13013122-d69f-4d6b-gu1d-721f190c4479"
+
+    mock_totalmobile_service = create_autospec(TotalmobileService)
+    mock_blaise_service = create_autospec(BlaiseService)
+
+    mock_totalmobile_service.get_jobs.return_value = [
+        {
+            "visitComplete": True,
+            "identity": {"reference": "LMS1111-AA1.12345"}
+        },
+        {
+            "visitComplete": False,
+            "identity": {"reference": "LMS2222-BB2.22222"}
+        },
+        {
+            "visitComplete": False,
+            "identity": {"reference": "LMS1111-AA1.67890"}
+        },
+        {
+            "visitComplete": False,
+            "identity": {"reference": "LMS2222-BB2.44444"}
+        },
+    ]
+    mock_blaise_service.get_case_status_information.side_effect = [
+        [
+            {
+                "primaryKey": "12345",
+                "outcome": 310
+            },
+            {
+                "primaryKey": "22222",
+                "outcome": 110
+            },
+            {
+                "primaryKey": "67890",
+                "outcome": 310
+            },
+            {
+                "primaryKey": "44444",
+                "outcome": 110
+            }
+        ],
+        [
+            {
+                "primaryKey": "12345",
+                "outcome": 310
+            },
+            {
+                "primaryKey": "22222",
+                "outcome": 310
+            },
+            {
+                "primaryKey": "67890",
+                "outcome": 110
+            }
+        ],
+    ]
+
+    delete_totalmobile_jobs_service = DeleteTotalmobileJobsService(mock_totalmobile_service, mock_blaise_service)
+
+    # act
+    delete_totalmobile_jobs_service.delete_totalmobile_jobs_completed_in_blaise()
+
+    # assert
+    mock_totalmobile_service.delete_job.assert_any_call(world_id, "LMS2222-BB2.22222", "110")
+    mock_totalmobile_service.delete_job.assert_any_call(world_id, "LMS1111-AA1.67890", "110")
+    mock_totalmobile_service.delete_job.assert_any_call(world_id, "LMS2222-BB2.44444", "110")
+
+    mock_blaise_service.get_case_status_information.assert_has_calls([call("LMS2222_BB2"), call("LMS1111_AA1")])
