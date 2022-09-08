71c6d164e8564bb82aae9a48ed36323fb1fb6f95
jamie kerr <jamiestephenkerr@gmail.com>
2022-09-08 17:25:28 +0100

    add totalmobile_job model

---
+++ tests/models/totalmobile/test_totalmobile_jobs_model.py
@@ -0,0 +1,26 @@
+from models.totalmobile.totalmobile_jobs_model import TotalmobileJobsModel
+
+
+def test_total_mobile_job_models_maps_expected_list_of_models_from_job_reference_list():
+    # arrange
+    job_references = [
+        "LMS1111-AA1.12345",
+        "LMS1111-AA1.67890",
+        "LMS2222-BB2.23384"
+    ]
+
+    # act
+    result = TotalmobileJobsModel.import_job_references(job_references)
+
+    # assert
+    assert len(result.questionnaire_jobs) == 2
+
+    assert len(result.questionnaire_jobs["LMS1111_AA1"]) == 2
+    assert result.questionnaire_jobs["LMS1111_AA1"][0].case_id == "12345"
+    assert result.questionnaire_jobs["LMS1111_AA1"][0].reference == "LMS1111-AA1.12345"
+    assert result.questionnaire_jobs["LMS1111_AA1"][1].case_id == "67890"
+    assert result.questionnaire_jobs["LMS1111_AA1"][1].reference == "LMS1111-AA1.67890"
+
+    assert len(result.questionnaire_jobs["LMS2222_BB2"]) == 1
+    assert result.questionnaire_jobs["LMS2222_BB2"][0].case_id == "23384"
+    assert result.questionnaire_jobs["LMS2222_BB2"][0].reference == "LMS2222-BB2.23384"
