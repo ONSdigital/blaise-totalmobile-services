c79d9b1a368b4ffed21b4fd7e0ab1ae4651745c7
jamie kerr <jamiestephenkerr@gmail.com>
2022-09-29 17:18:15 +0100

    fix issue where datastore client was making a connection on init

--- services/datastore_service.py
+++ services/datastore_service.py
@@ -2,10 +2,8 @@ from google.cloud import datastore
 
 
 class DatastoreService:
-    def __init__(self):
-        self._datastore_client = datastore.Client()
-        pass
 
     def get_totalmobile_release_date_records(self) -> list:
-        query = self._datastore_client.query(kind="TmReleaseDate")
+        datastore_client = datastore.Client()
+        query = datastore_client.query(kind="TmReleaseDate")
         return list(query.fetch())
