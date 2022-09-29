dae785189c0329d5f59821445376f9a09f3144f0
jamie kerr <jamiestephenkerr@gmail.com>
2022-09-29 17:13:06 +0100

    fix issue where bus client was making a connection on init

--- services/uac_service.py
+++ services/uac_service.py
@@ -6,13 +6,11 @@ from models.blaise.questionnaire_uac_model import QuestionnaireUacModel
 class UacService:
     def __init__(self, config: Config):
         self._config = config
-        self.bus_client = BusClient(
-            self._config.bus_api_url, self._config.bus_client_id
-        )
 
     def get_questionnaire_uac_model(
         self, questionnaire_name: str
     ) -> QuestionnaireUacModel:
-        uac_data_dictionary = self.bus_client.get_uacs_by_case_id(questionnaire_name)
+        bus_client = BusClient(self._config.bus_api_url, self._config.bus_client_id)
+        uac_data_dictionary = bus_client.get_uacs_by_case_id(questionnaire_name)
 
         return QuestionnaireUacModel.import_uac_data(uac_data_dictionary)
