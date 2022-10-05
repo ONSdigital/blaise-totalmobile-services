35f3ae9af100a27719c27379f5763b19591a70d9
jamie kerr <jamiestephenkerr@gmail.com>
2022-10-05 16:27:02 +0100

    added world id to log

--- services/delete_totalmobile_jobs_service.py
+++ services/delete_totalmobile_jobs_service.py
@@ -51,7 +51,9 @@ class DeleteTotalmobileJobsService:
 
     def _delete_jobs_past_field_period(self, jobs: List[Job], world_id: str):
         for job in jobs:
-            logging.info(f"job with case ID {job.case_id} has past field period value of {job.past_field_period}")
+            logging.info(
+                f"job with case ID {job.case_id} has past field period value of {job.past_field_period}"
+            )
             if job.past_field_period:
                 self._delete_job(world_id, job.reference, "past field period")
 
@@ -103,7 +105,7 @@ class DeleteTotalmobileJobsService:
         try:
             jobs_model = self._totalmobile_service.get_jobs_model(world_id)
             logging.info(
-                f"Found {jobs_model.total_number_of_incomplete_jobs()} incomplete jobs in totalmobile"
+                f"Found {jobs_model.total_number_of_incomplete_jobs()} incomplete jobs in totalmobile for world {world_id}"
             )
 
             return jobs_model.questionnaires_with_incomplete_jobs()
