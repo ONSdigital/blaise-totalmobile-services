5e8c56fc85de388871ad2ff3f251f04084636d5b
Tom Oram <tom.oram@armakuni.com>
2022-10-18 10:14:10 +0100

    refactor: Rename private property
    Refs: BLAIS5-3411

--- services/totalmobile_service.py
+++ services/totalmobile_service.py
@@ -1,3 +1,5 @@
+import requests
+
 from client.optimise import GetJobsResponse, OptimiseClient
 from models.cloud_tasks.totalmobile_create_job_model import TotalmobileCreateJobModel
 from models.totalmobile.totalmobile_get_jobs_response_model import (
@@ -7,21 +9,24 @@ from models.totalmobile.totalmobile_world_model import TotalmobileWorldModel
 
 
 class TotalmobileService:
-    def __init__(self, client: OptimiseClient):
-        self.client = client
+    def __init__(self, optimise_client: OptimiseClient):
+        self._optimise_client = optimise_client
 
     def get_world_model(self) -> TotalmobileWorldModel:
-        worlds = self.client.get_worlds()
+        worlds = self._optimise_client.get_worlds()
         return TotalmobileWorldModel.import_worlds(worlds)
 
-    def create_job(self, job: TotalmobileCreateJobModel):
-        return self.client.create_job(job.world_id, job.payload)
+    def create_job(self, job: TotalmobileCreateJobModel) -> requests.Response:
+        return self._optimise_client.create_job(job.world_id, job.payload)
 
-    def delete_job(self, world_id: str, job: str, reason: str = "0"):
-        return self.client.delete_job(world_id, job, reason)
+    def recall_job(self, reference: str) -> None:
+        raise NotImplementedError("TODO")
+
+    def delete_job(self, world_id: str, job: str, reason: str = "0") -> requests.Response:
+        return self._optimise_client.delete_job(world_id, job, reason)
 
     def get_jobs(self, world_id: str) -> GetJobsResponse:
-        return self.client.get_jobs(world_id)
+        return self._optimise_client.get_jobs(world_id)
 
     def get_jobs_model(self, world_id: str) -> TotalmobileGetJobsResponseModel:
         jobs_response = self.get_jobs(world_id)
