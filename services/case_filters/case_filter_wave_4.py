93c4aba51f7b34d6228c2c957873b51a634ec532
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2022-10-24 15:38:24 +0100

    feat: add wave 4 and 5 filtering
    refs: BLAIS5-3386

---
+++ services/case_filters/case_filter_wave_4.py
@@ -0,0 +1,17 @@
+from models.blaise.blaise_case_information_model import BlaiseCaseInformationModel
+from services.case_filters.case_filter_base import CaseFilterBase
+
+
+class CaseFilterWave4(CaseFilterBase):
+    def __init__(self):
+        self.valid_outcome_codes = [0]
+
+    @property
+    def wave_number(self) -> int:
+        return 4
+
+    def case_is_eligible(self, case: BlaiseCaseInformationModel) -> bool:
+        return (
+                self.case_is_part_of_wave(self.wave_number, case)
+                and self.case_has_a_desired_outcome_code_of(self.valid_outcome_codes, case)
+        )
