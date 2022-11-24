183b289430f8a59579442783eae3b5064a9f1c58
jamie kerr <jamiestephenkerr@gmail.com>
2022-11-24 11:37:37 +0000

    updated cloud function entr

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def create_totalmobile_jobs_processor(request: flask.Request) -> str:
     )
 
 
-def delete_totalmobile_jobs_completed_in_blaise(_event, _context) -> str:
+def delete_totalmobile_jobs_completed_in_blaise(_request: flask.Request) -> str:
     config = Config.from_env()
     totalmobile_service = _create_totalmobile_service(config=config)
     blaise_outcome_service = BlaiseCaseOutcomeService(
