612553029425d385734a1cadaa30e3dd56347bf8
elthorne <46898565+elthorne@users.noreply.github.com>
2023-03-09 12:04:25 +0000

    Feat: New requirement to check k2n indicator against cases withOUT telephone numbers...
    ...functionality and unit-tests updated.
    
    Refs: BLAIS5-3676

--- services/case_filters/case_filter_wave_2.py
+++ services/case_filters/case_filter_wave_2.py
@@ -18,15 +18,6 @@ class CaseFilterWave2(CaseFilterBase):
             self.case_has_field_case_of_y(case)
             and self.case_has_a_desired_outcome_code_of(self.valid_outcome_codes, case)
             and self.case_has_a_desired_rotational_outcome_code_of(
-                self.valid_rotational_outcome_codes, case
-            )
-            and self.case_is_eligible_if_telephone_numbers_are_set(case)
+                self.valid_rotational_outcome_codes, case)
+            and self.case_has_rotational_knock_to_nudge_indicator_of_empty_or_n(case)
         )
-
-    def case_is_eligible_if_telephone_numbers_are_set(
-        self, case: BlaiseCaseInformationModel
-    ) -> bool:
-        if self.case_has_telephone_numbers(case):
-            return self.case_has_rotational_knock_to_nudge_indicator_of_empty_or_n(case)
-
-        return True
