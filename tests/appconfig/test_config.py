c3cdf74e72d1d44c5f6d0a25a3582f7f4e7efa5a
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2023-11-01 09:47:53 +0000

    fml

--- tests/appconfig/test_config.py
+++ tests/appconfig/test_config.py
@@ -1,24 +1,5 @@
 from appconfig import Config
 
-
-# def test_config_from_env():
-#     config = Config.from_env()
-#     assert config == Config(
-#         totalmobile_url="",
-#         totalmobile_instance="",
-#         totalmobile_client_id="",
-#         totalmobile_client_secret="",
-#         create_totalmobile_jobs_task_queue_id="",
-#         gcloud_project="",
-#         region="",
-#         blaise_api_url="",
-#         blaise_server_park="",
-#         cloud_function_sa="",
-#         bus_api_url="",
-#         bus_client_id="",
-#     )
-
-
 import pytest
 import os
 
