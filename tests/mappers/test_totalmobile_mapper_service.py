83ff3e571051d4196adcc58d97327ce2d27d0269
jamie kerr <jamiestephenkerr@gmail.com>
2024-07-31 13:21:10 +0100

    updated mapper tests

--- tests/mappers/test_totalmobile_mapper_service.py
+++ tests/mappers/test_totalmobile_mapper_service.py
@@ -2,8 +2,8 @@ from typing import Dict
 from unittest.mock import Mock
 import pytest
 from client.bus import Uac
-from models.blaise.questionnaire_uac_model import QuestionnaireUacModel
-from models.totalmobile.totalmobile_outgoing_create_job_payload_model import TotalMobileOutgoingCreateJobPayloadModel
+from models.blaise.blaise_case_information_model import BlaiseCaseInformationModel
+from models.blaise.questionnaire_uac_model import QuestionnaireUacModel, UacChunks
 from models.totalmobile.totalmobile_world_model import TotalmobileWorldModel, World
 from services.mappers.totalmobile_mapper_service import TotalmobileMapperService
 from tests.helpers.get_blaise_case_model_helper import get_populated_case_model
@@ -24,6 +24,51 @@ def service(
 
 
 class TestMapTotalmobileJobModelsForLMS:
+    def totalmobile_payload_helper(
+            self,
+            questionnaire_name: str,
+            case: BlaiseCaseInformationModel,
+            uac_chunks: UacChunks | None) -> Dict[str, str]:
+
+        payload_dictionary = {'additionalProperties': [
+            {'name': 'surveyName', 'value': case.data_model_name},
+            {'name': 'tla', 'value': case.tla},
+            {'name': 'wave', 'value': f'{case.wave}'},
+            {'name': 'priority', 'value': case.priority},
+            {'name': 'fieldRegion', 'value': case.field_region},
+            {'name': 'fieldTeam', 'value': case.field_team}],
+            'attributes': [{'name': 'Region', 'value': case.field_region},
+                           {'name': 'Team', 'value': case.field_team}],
+            'contact': {'name': case.address_details.address.postcode},
+            'description': f'UAC: {uac_chunks.formatted_chunks() if uac_chunks is not None else ""}\n'
+                           f'Due Date: {case.wave_com_dte.strftime("%d/%m/%Y")}\n'
+                           f'Study: {questionnaire_name}\n'
+                           f'Case ID: {case.case_id}\n'
+                           f'Wave: {case.wave}',
+            'dueDate': {'end': case.wave_com_dte.strftime("%Y-%m-%d")},
+            'duration': 15,
+            'identity': {'reference': f'{questionnaire_name.replace("_", "-")}.{case.case_id}'},
+            'location': {'address': '12 Blaise Street, Blaise Hill, Blaiseville, Newport, '
+                                    'FML134D',
+                         'addressDetail': {'addressLine1': '12 Blaise Street, Blaise Hill',
+                                           'addressLine2': 'Blaiseville',
+                                           'addressLine3': 'Gwent',
+                                           'addressLine4': 'Newport',
+                                           'coordinates': {'latitude': '10020202',
+                                                           'longitude': '34949494'},
+                                           'postCode': 'FML134D'},
+                         'reference': case.address_details.reference},
+            'origin': 'ONS',
+            'skills': [{'identity': {'reference': case.tla}}],
+            'workType': case.tla}
+
+        if uac_chunks is not None:
+            payload_dictionary['additionalProperties'].append({'name': 'uac1', 'value': uac_chunks.uac1})
+            payload_dictionary['additionalProperties'].append({'name': 'uac2', 'value': uac_chunks.uac2})
+            payload_dictionary['additionalProperties'].append({'name': 'uac3', 'value': uac_chunks.uac3})
+
+        return payload_dictionary
+
     def test_map_totalmobile_job_models_maps_the_correct_list_of_models(
             self, mock_uac_service, service: TotalmobileMapperService
     ):
@@ -89,40 +134,28 @@ class TestMapTotalmobileJobModelsForLMS:
         assert result[0].questionnaire == "LMS2101_AA1"
         assert result[0].world_id == "3fa85f64-5717-4562-b3fc-2c963f66afa6"
         assert result[0].case_id == "10010"
-        assert (
-                result[0].payload
-                == TotalMobileOutgoingCreateJobPayloadModel.import_case(
-            questionnaire_name,
-            case_data[0],
-            questionnaire_uac_model.get_uac_chunks("10010"),
-        ).to_payload()
-        )
+        assert result[0].payload == self.totalmobile_payload_helper(
+            questionnaire_name=result[0].questionnaire,
+            case=case_data[0],
+            uac_chunks=questionnaire_uac_model.get_uac_chunks(case_data[0].case_id))
         assert result[0].payload["description"].startswith("UAC: 8175 4725 3990")
 
         assert result[1].questionnaire == "LMS2101_AA1"
         assert result[1].world_id == "3fa85f64-5717-4562-b3fc-2c963f66afa7"
         assert result[1].case_id == "10020"
-        assert (
-                result[1].payload
-                == TotalMobileOutgoingCreateJobPayloadModel.import_case(
-            questionnaire_name,
-            case_data[1],
-            questionnaire_uac_model.get_uac_chunks("10020"),
-        ).to_payload()
-        )
+        assert result[1].payload == self.totalmobile_payload_helper(
+            questionnaire_name=result[1].questionnaire,
+            case=case_data[1],
+            uac_chunks=questionnaire_uac_model.get_uac_chunks(case_data[1].case_id))
         assert result[1].payload["description"].startswith("UAC: 4175 5725 6990")
 
         assert result[2].questionnaire == "LMS2101_AA1"
         assert result[2].world_id == "3fa85f64-5717-4562-b3fc-2c963f66afa9"
         assert result[2].case_id == "10030"
-        assert (
-                result[2].payload
-                == TotalMobileOutgoingCreateJobPayloadModel.import_case(
-            questionnaire_name,
-            case_data[2],
-            questionnaire_uac_model.get_uac_chunks("10030"),
-        ).to_payload()
-        )
+        assert result[2].payload == self.totalmobile_payload_helper(
+            questionnaire_name=result[2].questionnaire,
+            case=case_data[2],
+            uac_chunks=None)
         assert result[2].payload["description"].startswith("UAC: \nDue Date")
 
 
@@ -133,7 +166,6 @@ class TestMapTotalmobileJobModelsForFRS:
         # arrange
         questionnaire_name = "FRS2101"
 
-        # TODO: get_populated_frs_case_model? Or does not matter because UAC is optional?
         case_data = [
             get_populated_case_model(
                 case_id="10010", field_region="region1"
@@ -146,10 +178,6 @@ class TestMapTotalmobileJobModelsForFRS:
             ),
         ]
 
-        uac_data_dictionary: Dict[str, Uac] = {}
-        questionnaire_uac_model = QuestionnaireUacModel.import_uac_data(uac_data_dictionary)
-        mock_uac_service.get_questionnaire_uac_model.return_value = questionnaire_uac_model
-
         world_model = TotalmobileWorldModel(
             worlds=[
                 World(region="region1", id="3fa85f64-5717-4562-b3fc-2c963f66afa6"),
@@ -157,6 +185,8 @@ class TestMapTotalmobileJobModelsForFRS:
                 World(region="region3", id="3fa85f64-5717-4562-b3fc-2c963f66afa9"),
             ]
         )
+
+        expected_payload = None
 
         # act
         result = service.map_totalmobile_create_job_models(
@@ -169,40 +199,20 @@ class TestMapTotalmobileJobModelsForFRS:
         assert result[0].questionnaire == "FRS2101"
         assert result[0].world_id == "3fa85f64-5717-4562-b3fc-2c963f66afa6"
         assert result[0].case_id == "10010"
-        assert (
-                result[0].payload
-                == TotalMobileOutgoingCreateJobPayloadModel.import_case(
-            questionnaire_name,
-            case_data[0],
-            questionnaire_uac_model.get_uac_chunks("10010"),
-        ).to_payload()
-        )
+        assert result[0].payload == expected_payload
         # TODO: Confirm w/Martyn requirements for 'Description'
         assert result[0].payload["description"].startswith("UAC: \nDue Date")
 
         assert result[1].questionnaire == "FRS2101"
         assert result[1].world_id == "3fa85f64-5717-4562-b3fc-2c963f66afa7"
         assert result[1].case_id == "10020"
-        assert (
-                result[1].payload
-                == TotalMobileOutgoingCreateJobPayloadModel.import_case(
-            questionnaire_name,
-            case_data[1],
-            questionnaire_uac_model.get_uac_chunks("10020"),
-        ).to_payload()
-        )
+        assert result[1].payload == expected_payload
+
         # TODO: Confirm w/Martyn requirements for 'Description'
         assert result[1].payload["description"].startswith("UAC: \nDue Date")
 
         assert result[2].questionnaire == "FRS2101"
         assert result[2].world_id == "3fa85f64-5717-4562-b3fc-2c963f66afa9"
         assert result[2].case_id == "10030"
-        assert (
-                result[2].payload
-                == TotalMobileOutgoingCreateJobPayloadModel.import_case(
-            questionnaire_name,
-            case_data[2],
-            questionnaire_uac_model.get_uac_chunks("10030"),
-        ).to_payload()
-        )
+        assert result[2].payload == expected_payload
         assert result[2].payload["description"].startswith("UAC: \nDue Date")
