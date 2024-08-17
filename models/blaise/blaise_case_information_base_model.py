a11d002f6d7ac944e73d2970422f632db32b6c4b
jamie kerr <jamiestephenkerr@gmail.com>
2024-08-17 13:17:39 +0100

    reformatted

--- models/blaise/blaise_case_information_base_model.py
+++ models/blaise/blaise_case_information_base_model.py
@@ -2,8 +2,9 @@ from abc import abstractmethod
 from dataclasses import dataclass
 from datetime import datetime
 from typing import List, Optional
-from models.blaise.questionnaire_uac_model import UacChunks
+
 from models.base_model import BaseModel
+from models.blaise.questionnaire_uac_model import UacChunks
 
 
 @dataclass
