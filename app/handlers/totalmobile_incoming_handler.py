2d7437602f5da046a7939a9c8b94010fac223a23
SidraJaved <sidrajaved92@gmail.com>
2024-09-27 13:22:43 +0100

    printing payload to check TM payload structure
    Refs : BLAIS5-4428

--- app/handlers/totalmobile_incoming_handler.py
+++ app/handlers/totalmobile_incoming_handler.py
@@ -16,6 +16,7 @@ def submit_form_result_request_handler(request, update_case_service: UpdateCaseS
 
 def create_visit_request_handler(request, update_frs_case_allocation_service: UpdateFRSCaseService):
     data = request.get_json()
+    logging.info(str(data))
     totalmobile_frs_case = TotalMobileIncomingUpdateFRSRequestModel.import_request(data)
     logging.info(str(totalmobile_frs_case))
     update_frs_case_allocation_service.update_case(totalmobile_frs_case)
