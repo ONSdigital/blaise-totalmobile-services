88f1c8d1c2408a47642ca94bd4afa0fe9d8d232c
Elinor <46898565+elthorne@users.noreply.github.com>
2025-02-03 10:32:06 +0000

    test: update_case_contact_information()
    refs: BLAIS5-4351

--- tests/services/update/test_lms_update_case_service.py
+++ tests/services/update/test_lms_update_case_service.py
@@ -89,3 +89,33 @@ def test_update_case_calls_should_update_case_contact_information_once_with_corr
     mock_case_update_service._should_update_case_contact_information.assert_called_once_with(
         mock_blaise_case, mock_totalmobile_request
     )
+
+
+@patch.object(LMSUpdateCaseService, "get_existing_blaise_case")
+@patch.object(LMSUpdateCaseService, "_should_update_case_contact_information")
+def test_update_case_calls_update_case_contact_information_once_with_correct_parameters(
+    mock_should_update_case_contact_information,
+    mock_get_existing_blaise_case,
+    mock_case_update_service,
+):
+    # arrange
+    mock_case_update_service._update_case_contact_information = Mock()
+    mock_totalmobile_request = lms_totalmobile_incoming_update_request_helper()
+    questionnaire_name = "LMS2101_AA1"
+    case_id = "90001"
+    case_data = {
+        BlaiseFields.case_id: case_id,
+        BlaiseFields.outcome_code: 0,
+        BlaiseFields.call_history: False,
+    }
+    mock_blaise_case = BlaiseUpdateCase(questionnaire_name, case_data)
+    mock_get_existing_blaise_case.return_value = mock_blaise_case
+    mock_should_update_case_contact_information.return_value = True
+
+    # act
+    mock_case_update_service.update_case(mock_totalmobile_request)
+
+    # assert
+    mock_case_update_service._update_case_contact_information.assert_called_once_with(
+        mock_totalmobile_request, mock_blaise_case
+    )
