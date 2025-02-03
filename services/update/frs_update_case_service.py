f26bd02dcb616a891175c08cec843d3b9c6cac47
Elinor <46898565+elthorne@users.noreply.github.com>
2025-02-03 12:11:26 +0000

    feat: FRS update case service
    starter for ten
    
    refs: BLAIS5-4351

--- services/update/frs_update_case_service.py
+++ services/update/frs_update_case_service.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Dict
 
 from enums.questionnaire_case_outcome_codes import FRSQuestionnaireOutcomeCodes
 from models.update.blaise_update_case_model import BlaiseUpdateCase
@@ -23,17 +24,11 @@ class FRSUpdateCaseService(UpdateCaseServiceBase):
             totalmobile_request.questionnaire_name, totalmobile_request.case_id
         )
 
-        if totalmobile_request.outcome_code in {
-            enum.value for enum in FRSQuestionnaireOutcomeCodes
-        }:
+        if self._should_update_case_outcome_code(totalmobile_request):
             self._update_case_outcome_code(totalmobile_request, blaise_case)
             return
 
-        logging.info(
-            f"Case {totalmobile_request.case_id} for questionnaire {totalmobile_request.questionnaire_name} "
-            f"has not been updated in Blaise (Blaise hOut={blaise_case.outcome_code}, "
-            f"TM hOut={totalmobile_request.outcome_code})"
-        )
+        self._log_no_update(blaise_case, totalmobile_request)
 
     def _update_case_outcome_code(
         self,
@@ -41,13 +36,11 @@ class FRSUpdateCaseService(UpdateCaseServiceBase):
         blaise_case: BlaiseUpdateCase,
     ) -> None:
 
-        fields_to_update = {}
-
-        fields_to_update.update(
-            # TODO - this needs adjusting for FRS
-            # TODO - I need me some base and children classes
-            blaise_case.get_outcome_code_fields(totalmobile_request)
+        fields_to_update = self._get_fields_to_update_case_outcome_code(
+            blaise_case, totalmobile_request
         )
+
+        self._log_attempting_to_update_case(totalmobile_request)
 
         self._blaise_service.update_case(
             totalmobile_request.questionnaire_name,
@@ -55,8 +48,51 @@ class FRSUpdateCaseService(UpdateCaseServiceBase):
             fields_to_update,
         )
 
+        self._log_outcome_code_updated(
+            blaise_case, totalmobile_request
+        )
+
+    @staticmethod
+    def _should_update_case_outcome_code(
+        totalmobile_request: TotalMobileIncomingUpdateRequestModel,
+    ) -> bool:
+        return totalmobile_request.outcome_code in {
+            enum.value for enum in FRSQuestionnaireOutcomeCodes
+        }
+
+    @staticmethod
+    def _get_fields_to_update_case_outcome_code(
+        blaise_case: BlaiseUpdateCase,
+        totalmobile_request: TotalMobileIncomingUpdateRequestModel,
+    ) -> Dict[str, str]:
+        return {**blaise_case.get_outcome_code_fields(totalmobile_request)}
+
+    @staticmethod
+    def _log_outcome_code_updated(
+        blaise_case: BlaiseUpdateCase,
+        totalmobile_request: TotalMobileIncomingUpdateRequestModel,
+    ) -> None:
         logging.info(
             f"Outcome code updated (Questionnaire={totalmobile_request.questionnaire_name}, "
             f"Case Id={blaise_case.case_id}, Blaise hOut={blaise_case.outcome_code}, "
+            f"TM hOut={totalmobile_request.outcome_code})"
+        )
+
+    @staticmethod
+    def _log_attempting_to_update_case(
+        totalmobile_request: TotalMobileIncomingUpdateRequestModel,
+    ) -> None:
+        logging.info(
+            f"Attempting to update case {totalmobile_request.case_id} in questionnaire {totalmobile_request.questionnaire_name} in Blaise"
+        )
+
+    @staticmethod
+    def _log_no_update(
+        blaise_case: BlaiseUpdateCase,
+        totalmobile_request: TotalMobileIncomingUpdateRequestModel,
+    ) -> None:
+        logging.info(
+            f"Case {totalmobile_request.case_id} for questionnaire {totalmobile_request.questionnaire_name} "
+            f"has not been updated in Blaise (Blaise hOut={blaise_case.outcome_code}, "
             f"TM hOut={totalmobile_request.outcome_code})"
         )
