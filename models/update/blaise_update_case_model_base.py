2c35bfb52bf84c8e18d3239491ee158743833f48
Elinor <46898565+elthorne@users.noreply.github.com>
2025-02-12 16:24:08 +0000

    refactor: resolve mypy issue
    refs: BLAIS5-4351

--- models/update/blaise_update_case_model_base.py
+++ models/update/blaise_update_case_model_base.py
@@ -11,6 +11,14 @@ class BlaiseUpdateCaseBase:
         self.questionnaire_name = questionnaire_name
         self._case_data = case_data
 
+    @property
+    def questionnaire_name(self) -> str:
+        return self._questionnaire_name
+
+    @questionnaire_name.setter
+    def questionnaire_name(self, value):
+        self._questionnaire_name = value
+
     @abstractmethod
     def get_outcome_code_fields(
         self,
