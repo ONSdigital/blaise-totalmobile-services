1eea86afa06224a9bcc90dca8f20e776545cde1f
Elinor <46898565+elthorne@users.noreply.github.com>
2025-02-26 15:17:31 +0000

    refactor: abrstract outcome enums to sets for efficient processing and improved readability
    refs: BLAIS5-4631

--- services/update/frs_update_case_service.py
+++ services/update/frs_update_case_service.py
@@ -33,51 +33,16 @@ class FRSUpdateCaseService(UpdateCaseServiceBase[FRSBlaiseUpdateCase]):
         # check with Martyn, what happens with Blaise?
 
         if totalmobile_request.outcome_code in (
-            FRSQuestionnaireOutcomeCodes.NO_CONTACT_WITH_ANYONE_AT_ADDRESS_310.value,
-            FRSQuestionnaireOutcomeCodes.CONTACT_MADE_AT_ADDRESS_NO_CONTACT_WITH_SAMPLED_HOUSEHOLD_MULTI_320.value,
-            FRSQuestionnaireOutcomeCodes.CONTACT_WITH_SAMPLED_HOUSEHOLD_NO_CONTACT_WITH_RESPONSIBLE_RESIDENT_330.value,
-            FRSQuestionnaireOutcomeCodes.ILL_AT_HOME_DURING_SURVEY_PERIOD_512.value,
-            FRSQuestionnaireOutcomeCodes.AWAY_IN_HOSPITAL_DURING_SURVEY_PERIOD_522.value,
-            FRSQuestionnaireOutcomeCodes.PHYSICALLY_MENTALLY_UNABLE_INCOMPETENT_532.value,
-            FRSQuestionnaireOutcomeCodes.LANGUAGE_DIFFICULTIES_542.value,
-            FRSQuestionnaireOutcomeCodes.NOT_ISSUED_TO_INTERVIEWER_OFFICE_APPROVAL_NEEDED_611.value,
-            FRSQuestionnaireOutcomeCodes.ISSUED_BUT_NOT_ATTEMPTED_OFFICE_APPROVAL_NEEDED_612.value,
-            FRSQuestionnaireOutcomeCodes.INACCESSIBLE_620.value,
-            FRSQuestionnaireOutcomeCodes.UNABLE_TO_LOCATE_ADDRESS_630.value,
-            FRSQuestionnaireOutcomeCodes.UNKNOWN_WHETHER_RESIDENTIAL_HOUSING_INFORMATION_REFUSED_641.value,
-            FRSQuestionnaireOutcomeCodes.UNKNOWN_WHETHER_RESIDENTIAL_HOUSING_NO_CONTACT_WITH_KNOWLEDGEABLE_PERSON_642.value,
-            FRSQuestionnaireOutcomeCodes.RESIDENTIAL_ADDRESS_UNKNOWN_IF_ELIGIBLE_HOUSEHOLD_INFORMATION_REFUSED_651.value,
-            FRSQuestionnaireOutcomeCodes.RESIDENTIAL_ADDRESS_UNKNOWN_IF_ELIGIBLE_HOUSEHOLD_NO_CONTACT_WITH_KNOWLEDGEABLE_PERSON_652.value,
-            FRSQuestionnaireOutcomeCodes.OTHER_UNKNOWN_ELIGIBILITY_OFFICE_APPROVAL_NEEDED_670.value,
-            FRSQuestionnaireOutcomeCodes.NOT_YET_BUILT_UNDER_CONSTRUCTION_710.value,
-            FRSQuestionnaireOutcomeCodes.DEMOLISHED_DERELICT_720.value,
-            FRSQuestionnaireOutcomeCodes.VACANT_EMPTY_730.value,
-            FRSQuestionnaireOutcomeCodes.NON_RESIDENTIAL_ADDRESS_740.value,
-            FRSQuestionnaireOutcomeCodes.ADDRESS_OCCUPIED_NO_RESIDENT_HOUSEHOLD_750.value,
-            FRSQuestionnaireOutcomeCodes.COMMUNAL_ESTABLISHMENT_INSTITUTION_760.value,
-            FRSQuestionnaireOutcomeCodes.DWELLING_OF_FOREIGN_SERVICE_PERSONNEL_DIPLOMATS_771.value,
-            FRSQuestionnaireOutcomeCodes.ALL_RESIDENTS_UNDER_16_772.value,
-            FRSQuestionnaireOutcomeCodes.OTHER_RESIDENT_HOUSEHOLD_NO_ELIGIBLE_RESIDENTS_773.value,
-            FRSQuestionnaireOutcomeCodes.DIRECTED_NOT_TO_SAMPLE_AT_ADDRESS_781.value,
-            FRSQuestionnaireOutcomeCodes.NOT_TO_INTERVIEW_INSTRUCTS_SCOTTISH_PRE_SELECTION_SHEET_782.value,
-            FRSQuestionnaireOutcomeCodes.HOUSEHOLD_LIMIT_ON_QUOTA_REACHED_MAXIMUM_OF_4_EXTRA_HOUSEHOLDS_783.value,
-            FRSQuestionnaireOutcomeCodes.OTHER_OFFICE_APPROVAL_NEEDED_790.value,
+            FRSQuestionnaireOutcomeCodes.outcome_update_set()
         ) and blaise_case.outcome_code not in (
-            FRSQuestionnaireOutcomeCodes.COMPLETED_110.value,
-            FRSQuestionnaireOutcomeCodes.PARTIALLY_COMPLETED_210.value,
+            FRSQuestionnaireOutcomeCodes.completed_set()
         ):
             fields_to_update.update(
                 blaise_case.get_outcome_code_fields(totalmobile_request)
             )
 
         if totalmobile_request.outcome_code in (
-            FRSQuestionnaireOutcomeCodes.HQ_OFFICE_REFUSAL_GENERAL_410.value,
-            FRSQuestionnaireOutcomeCodes.MULTI_INFORMATION_REFUSED_NO_OF_HOUSEHOLDS_AT_ADDRESS_420.value,
-            FRSQuestionnaireOutcomeCodes.REFUSAL_AT_INTRODUCTION_BEFORE_INTERVIEW_BY_ADULT_HOUSEHOLD_MEMBER_431.value,
-            FRSQuestionnaireOutcomeCodes.REFUSAL_AT_INTRODUCTION_BEFORE_INTERVIEW_BY_PROXY_432.value,
-            FRSQuestionnaireOutcomeCodes.REFUSAL_DURING_INTERVIEW_HRP_BU_MEMBER_REFUSED_TO_COMPLETE_INTERVIEW_441.value,
-            FRSQuestionnaireOutcomeCodes.REFUSAL_DURING_INTERVIEW_12_PLUS_DKS_OR_REFUSALS_IN_HHLD_SECTION_HRP_BU_442.value,
-            FRSQuestionnaireOutcomeCodes.BROKEN_APPOINTMENT_NO_RE_CONTACT_450.value,
+            FRSQuestionnaireOutcomeCodes.refusal_reason_set()
         ):
             # TODO: Remove from CMA
 
