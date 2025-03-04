f90f6f6883f228a42e4d3e2b988de869283a0346
Elinor <46898565+elthorne@users.noreply.github.com>
2025-03-04 09:36:04 +0000

    refactor: attempt to make environment.py more readable...
    I'm not sure if it's better or worse now!?
    
    refs: BLAIS5-4631

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -1,42 +1,58 @@
 from werkzeug.security import generate_password_hash
-
 from app.app import setup_app
 from services.delete.blaise_case_outcome_service import BlaiseCaseOutcomeService
 from tests.fakes.fake_blaise_service import FakeBlaiseService
+from tests.fakes.fake_cma_service import FakeCMAService
 from tests.fakes.fake_datastore_service import FakeDatastoreService
 from tests.fakes.fake_totalmobile_service import FakeTotalmobileService
 from tests.fakes.fake_uac_service import FakeUacService
 
 
 def before_scenario(context, _scenario):
-    app = setup_test_app()
-    setup_context(app, context)
+    """Entrypoint"""
+    context.app = setup_test_app()
+    setup_context(context)
 
 
 def setup_test_app():
     app = setup_app()
 
-    app.blaise_service = FakeBlaiseService()
-    app.uac_service = FakeUacService()
-    app.totalmobile_service = FakeTotalmobileService()
-
-    app.config["user"] = "test_username"
-    app.config["password_hash"] = generate_password_hash("test_password")
+    assign_fake_services_to_app(app)
+    configure_app_authentication(app)
 
     return app
 
 
-def setup_context(app, context):
-    setup_test_services(app, context)
-    context.test_client = app.test_client()
-    context.app = app
+def setup_context(context):
+    context.test_client = context.app.test_client()
+
+    assign_app_service_to_context(context)
+    assign_additional_services_to_context(context)
 
 
-def setup_test_services(app, context):
-    context.blaise_service = app.blaise_service
-    context.cma_service = app.cma_blaise_service
-    context.uac_service = app.uac_service
-    context.totalmobile_service = app.totalmobile_service
+def configure_app_authentication(app):
+    app.config.update(
+        {
+            "user": "test_username",
+            "password_hash": generate_password_hash("test_password"),
+        }
+    )
 
+
+def assign_fake_services_to_app(app):
+    app.blaise_service = FakeBlaiseService()
+    app.cma_service = FakeCMAService()
+    app.uac_service = FakeUacService()
+    app.totalmobile_service = FakeTotalmobileService()
+
+
+def assign_additional_services_to_context(context):
     context.datastore_service = FakeDatastoreService()
     context.blaise_outcome_service = BlaiseCaseOutcomeService(context.blaise_service)
+
+
+def assign_app_service_to_context(context):
+    context.blaise_service = context.app.blaise_service
+    context.cma_service = context.app.cma_service
+    context.uac_service = context.app.uac_service
+    context.totalmobile_service = context.app.totalmobile_service
