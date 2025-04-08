7581f947f9f3041db16a0541de4a4bdd65f09388
Elinor <46898565+elthorne@users.noreply.github.com>
2025-04-08 12:19:28 +0100

    fix: Return empty string to the Case Note column in CMA
    refs: BLAIS5-4698

--- tests/models/create/cma/test_blaise_cma_frs_create_case_model.py
+++ tests/models/create/cma/test_blaise_cma_frs_create_case_model.py
@@ -42,5 +42,5 @@ def test_format_data_fields_returns_a_correctly_formatted_data_fields_for_frs_ca
         "cmA_CustomUse": "",
         "cmA_Location": "",
         "cmA_InPossession": "",
-        "cmA_ContactData": f"PII.TLA\tFRS\tPII.Month\tOctober\tPII.Year\t2024\tPII.Prem1\tprem1\tPII.Prem2\tprem2\tPII.Town\ttown\tPII.Postcode\tNP10 8XG",
+        "cmA_ContactData": f"PII.TLA\tFRS\tPII.Month\tOctober\tPII.Year\t2024\tPII.Prem1\tprem1\tPII.Prem2\tprem2\tPII.Town\ttown\tPII.Postcode\tNP10 8XG\tCaseNote\t",
     }
