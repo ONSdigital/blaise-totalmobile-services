1b522738a02529adbb94f888d41300351af6d1f6
lambeb <141648830+lambeb@users.noreply.github.com>
2025-04-15 13:48:24 +0100

    Fixed behavioural tests

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -3,8 +3,8 @@ from unittest.mock import MagicMock
 from werkzeug.security import generate_password_hash
 
 from app.app import setup_app
+from services.case_instruction_service import CaseInstructionService
 from services.cma_blaise_service import CMABlaiseService
-from services.create.cma.allocate_cma_case_service import AllocateCMACaseService
 from services.delete.blaise_case_outcome_service import BlaiseCaseOutcomeService
 from services.delete.delete_cma_case_service import DeleteCMACaseService
 from tests.fakes.fake_blaise_service import FakeBlaiseService
@@ -59,7 +59,7 @@ def assign_additional_services_to_context(context):
     context.mock_cma_blaise_service = MagicMock(spec=CMABlaiseService)
     context.mock_delete_service = DeleteCMACaseService(
         cma_blaise_service=context.mock_cma_blaise_service,
-        allocate_cma_case_service=MagicMock(spec=AllocateCMACaseService),
+        case_instruction_service=MagicMock(spec=CaseInstructionService),
     )
 
 
